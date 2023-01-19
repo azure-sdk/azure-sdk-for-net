e5ff7d44d3d4755fcdc180d576b272c875c75da1
SDKAuto <sdkautomation@microsoft.com>
2023-01-19 17:56:49 +0000

    CodeGen from PR 22176 in Azure/azure-rest-api-specs Merge abf2e66689874a0b07671c04455ba66f26c7ae53 into 4dc5391db0148a0f93a8fe496adcb64302f5d5c4

--- sdk/mediaservices/Azure.ResourceManager.Media/src/Generated/Models/MediaServicesAccountData.Serialization.cs
+++ sdk/mediaservices/Azure.ResourceManager.Media/src/Generated/Models/MediaServicesAccountData.Serialization.cs
@@ -83,6 +83,11 @@ namespace Azure.ResourceManager.Media
                     writer.WriteNull("publicNetworkAccess");
                 }
             }
+            if (Optional.IsDefined(MinimumTlsVersion))
+            {
+                writer.WritePropertyName("minimumTlsVersion");
+                writer.WriteStringValue(MinimumTlsVersion.Value.ToString());
+            }
             writer.WriteEndObject();
             writer.WriteEndObject();
         }
@@ -104,6 +109,7 @@ namespace Azure.ResourceManager.Media
             Optional<MediaServicesPublicNetworkAccess?> publicNetworkAccess = default;
             Optional<MediaServicesProvisioningState> provisioningState = default;
             Optional<IReadOnlyList<MediaServicesPrivateEndpointConnectionData>> privateEndpointConnections = default;
+            Optional<MinimumTlsVersion> minimumTlsVersion = default;
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("identity"))
@@ -260,11 +266,21 @@ namespace Azure.ResourceManager.Media
                             privateEndpointConnections = array;
                             continue;
                         }
+                        if (property0.NameEquals("minimumTlsVersion"))
+                        {
+                            if (property0.Value.ValueKind == JsonValueKind.Null)
+                            {
+                                property0.ThrowNonNullablePropertyIsNull();
+                                continue;
+                            }
+                            minimumTlsVersion = new MinimumTlsVersion(property0.Value.GetString());
+                            continue;
+                        }
                     }
                     continue;
                 }
             }
-            return new MediaServicesAccountData(id, name, type, systemData.Value, Optional.ToDictionary(tags), location, identity, Optional.ToNullable(mediaServiceId), Optional.ToList(storageAccounts), Optional.ToNullable(storageAuthentication), encryption.Value, keyDelivery.Value, Optional.ToNullable(publicNetworkAccess), Optional.ToNullable(provisioningState), Optional.ToList(privateEndpointConnections));
+            return new MediaServicesAccountData(id, name, type, systemData.Value, Optional.ToDictionary(tags), location, identity, Optional.ToNullable(mediaServiceId), Optional.ToList(storageAccounts), Optional.ToNullable(storageAuthentication), encryption.Value, keyDelivery.Value, Optional.ToNullable(publicNetworkAccess), Optional.ToNullable(provisioningState), Optional.ToList(privateEndpointConnections), Optional.ToNullable(minimumTlsVersion));
         }
     }
 }
