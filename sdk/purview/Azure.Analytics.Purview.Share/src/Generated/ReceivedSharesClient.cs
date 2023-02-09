f134e34ba894e05d2a7ffc7c92e8449e26d7d077
SDKAuto <sdkautomation@microsoft.com>
2023-02-09 05:03:19 +0000

    CodeGen from PR 22323 in Azure/azure-rest-api-specs Merge 8b98b427364867cc081bb01267888ae14e365123 into ec6a94a6a47e7db7a7255fa3d5df20e4ddf5c95a

--- sdk/purview/Azure.Analytics.Purview.Share/src/Generated/ReceivedSharesClient.cs
+++ sdk/purview/Azure.Analytics.Purview.Share/src/Generated/ReceivedSharesClient.cs
@@ -20,8 +20,7 @@ namespace Azure.Analytics.Purview.Share
         private static readonly string[] AuthorizationScopes = new string[] { "https://purview.azure.net/.default" };
         private readonly TokenCredential _tokenCredential;
         private readonly HttpPipeline _pipeline;
-        private readonly string _endpoint;
-        private readonly string _apiVersion;
+        private readonly Uri _endpoint;
 
         /// <summary> The ClientDiagnostics is used to provide tracing support for the client library. </summary>
         internal ClientDiagnostics ClientDiagnostics { get; }
@@ -35,29 +34,28 @@ namespace Azure.Analytics.Purview.Share
         }
 
         /// <summary> Initializes a new instance of ReceivedSharesClient. </summary>
-        /// <param name="endpoint"> The scanning endpoint of your purview account. Example: https://{accountName}.purview.azure.com/share. </param>
+        /// <param name="endpoint"> The account endpoint of your Purview account. Example: https://{accountName}.purview.azure.com/. </param>
         /// <param name="credential"> A credential used to authenticate to an Azure Service. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="endpoint"/> or <paramref name="credential"/> is null. </exception>
-        public ReceivedSharesClient(string endpoint, TokenCredential credential) : this(endpoint, credential, new PurviewShareClientOptions())
+        public ReceivedSharesClient(Uri endpoint, TokenCredential credential) : this(endpoint, credential, new PurviewPDSClientOptions())
         {
         }
 
         /// <summary> Initializes a new instance of ReceivedSharesClient. </summary>
-        /// <param name="endpoint"> The scanning endpoint of your purview account. Example: https://{accountName}.purview.azure.com/share. </param>
+        /// <param name="endpoint"> The account endpoint of your Purview account. Example: https://{accountName}.purview.azure.com/. </param>
         /// <param name="credential"> A credential used to authenticate to an Azure Service. </param>
         /// <param name="options"> The options for configuring the client. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="endpoint"/> or <paramref name="credential"/> is null. </exception>
-        public ReceivedSharesClient(string endpoint, TokenCredential credential, PurviewShareClientOptions options)
+        public ReceivedSharesClient(Uri endpoint, TokenCredential credential, PurviewPDSClientOptions options)
         {
             Argument.AssertNotNull(endpoint, nameof(endpoint));
             Argument.AssertNotNull(credential, nameof(credential));
-            options ??= new PurviewShareClientOptions();
+            options ??= new PurviewPDSClientOptions();
 
             ClientDiagnostics = new ClientDiagnostics(options, true);
             _tokenCredential = credential;
             _pipeline = HttpPipelineBuilder.Build(options, Array.Empty<HttpPipelinePolicy>(), new HttpPipelinePolicy[] { new BearerTokenAuthenticationPolicy(_tokenCredential, AuthorizationScopes) }, new ResponseClassifier());
             _endpoint = endpoint;
-            _apiVersion = options.Version;
         }
 
         /// <summary> Get a received share by name. </summary>
@@ -258,9 +256,9 @@ namespace Azure.Analytics.Purview.Share
             var request = message.Request;
             request.Method = RequestMethod.Get;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(_endpoint, false);
+            uri.Reset(_endpoint);
             uri.AppendPath("/receivedShares", false);
-            uri.AppendQuery("api-version", _apiVersion, true);
+            uri.AppendQuery("api-version", "2021-09-01-preview", true);
             if (skipToken != null)
             {
                 uri.AppendQuery("skipToken", skipToken, true);
@@ -284,10 +282,10 @@ namespace Azure.Analytics.Purview.Share
             var request = message.Request;
             request.Method = RequestMethod.Get;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(_endpoint, false);
+            uri.Reset(_endpoint);
             uri.AppendPath("/receivedShares/", false);
             uri.AppendPath(receivedShareName, true);
-            uri.AppendQuery("api-version", _apiVersion, true);
+            uri.AppendQuery("api-version", "2021-09-01-preview", true);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
             return message;
@@ -299,10 +297,10 @@ namespace Azure.Analytics.Purview.Share
             var request = message.Request;
             request.Method = RequestMethod.Put;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(_endpoint, false);
+            uri.Reset(_endpoint);
             uri.AppendPath("/receivedShares/", false);
             uri.AppendPath(receivedShareName, true);
-            uri.AppendQuery("api-version", _apiVersion, true);
+            uri.AppendQuery("api-version", "2021-09-01-preview", true);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
             request.Headers.Add("Content-Type", "application/json");
@@ -316,10 +314,10 @@ namespace Azure.Analytics.Purview.Share
             var request = message.Request;
             request.Method = RequestMethod.Delete;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(_endpoint, false);
+            uri.Reset(_endpoint);
             uri.AppendPath("/receivedShares/", false);
             uri.AppendPath(receivedShareName, true);
-            uri.AppendQuery("api-version", _apiVersion, true);
+            uri.AppendQuery("api-version", "2021-09-01-preview", true);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
             return message;
@@ -331,7 +329,7 @@ namespace Azure.Analytics.Purview.Share
             var request = message.Request;
             request.Method = RequestMethod.Get;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(_endpoint, false);
+            uri.Reset(_endpoint);
             uri.AppendRawNextLink(nextLink, false);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
