f6e14dae5cbe755779f630c7f26d7dfd9182bcc1
SDKAuto <sdkautomation@microsoft.com>
2023-02-10 03:18:53 +0000

    CodeGen from PR 22519 in Azure/azure-rest-api-specs Merge 44e194e9b01f101e6f98955a10aa9296b2017635 into 33a7dbc1743fdacb8e2259011a035154a98cad66

--- sdk/purview/Azure.Analytics.Purview.Share/src/Generated/ReceivedInvitationsClient.cs
+++ sdk/purview/Azure.Analytics.Purview.Share/src/Generated/ReceivedInvitationsClient.cs
@@ -20,8 +20,7 @@ namespace Azure.Analytics.Purview.Share
         private static readonly string[] AuthorizationScopes = new string[] { "https://purview.azure.net/.default" };
         private readonly TokenCredential _tokenCredential;
         private readonly HttpPipeline _pipeline;
-        private readonly string _endpoint;
-        private readonly string _apiVersion;
+        private readonly Uri _endpoint;
 
         /// <summary> The ClientDiagnostics is used to provide tracing support for the client library. </summary>
         internal ClientDiagnostics ClientDiagnostics { get; }
@@ -35,29 +34,28 @@ namespace Azure.Analytics.Purview.Share
         }
 
         /// <summary> Initializes a new instance of ReceivedInvitationsClient. </summary>
-        /// <param name="endpoint"> The scanning endpoint of your purview account. Example: https://{accountName}.purview.azure.com/share. </param>
+        /// <param name="endpoint"> The account endpoint of your Purview account. Example: https://{accountName}.purview.azure.com/. </param>
         /// <param name="credential"> A credential used to authenticate to an Azure Service. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="endpoint"/> or <paramref name="credential"/> is null. </exception>
-        public ReceivedInvitationsClient(string endpoint, TokenCredential credential) : this(endpoint, credential, new PurviewShareClientOptions())
+        public ReceivedInvitationsClient(Uri endpoint, TokenCredential credential) : this(endpoint, credential, new PurviewPDSClientOptions())
         {
         }
 
         /// <summary> Initializes a new instance of ReceivedInvitationsClient. </summary>
-        /// <param name="endpoint"> The scanning endpoint of your purview account. Example: https://{accountName}.purview.azure.com/share. </param>
+        /// <param name="endpoint"> The account endpoint of your Purview account. Example: https://{accountName}.purview.azure.com/. </param>
         /// <param name="credential"> A credential used to authenticate to an Azure Service. </param>
         /// <param name="options"> The options for configuring the client. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="endpoint"/> or <paramref name="credential"/> is null. </exception>
-        public ReceivedInvitationsClient(string endpoint, TokenCredential credential, PurviewShareClientOptions options)
+        public ReceivedInvitationsClient(Uri endpoint, TokenCredential credential, PurviewPDSClientOptions options)
         {
             Argument.AssertNotNull(endpoint, nameof(endpoint));
             Argument.AssertNotNull(credential, nameof(credential));
-            options ??= new PurviewShareClientOptions();
+            options ??= new PurviewPDSClientOptions();
 
             ClientDiagnostics = new ClientDiagnostics(options, true);
             _tokenCredential = credential;
             _pipeline = HttpPipelineBuilder.Build(options, Array.Empty<HttpPipelinePolicy>(), new HttpPipelinePolicy[] { new BearerTokenAuthenticationPolicy(_tokenCredential, AuthorizationScopes) }, new ResponseClassifier());
             _endpoint = endpoint;
-            _apiVersion = options.Version;
         }
 
         /// <summary> Gets the received invitation identified by name. </summary>
@@ -206,9 +204,9 @@ namespace Azure.Analytics.Purview.Share
             var request = message.Request;
             request.Method = RequestMethod.Get;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(_endpoint, false);
+            uri.Reset(_endpoint);
             uri.AppendPath("/receivedInvitations", false);
-            uri.AppendQuery("api-version", _apiVersion, true);
+            uri.AppendQuery("api-version", "2021-09-01-preview", true);
             if (skipToken != null)
             {
                 uri.AppendQuery("skipToken", skipToken, true);
@@ -232,10 +230,10 @@ namespace Azure.Analytics.Purview.Share
             var request = message.Request;
             request.Method = RequestMethod.Get;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(_endpoint, false);
+            uri.Reset(_endpoint);
             uri.AppendPath("/receivedInvitations/", false);
             uri.AppendPath(receivedInvitationName, true);
-            uri.AppendQuery("api-version", _apiVersion, true);
+            uri.AppendQuery("api-version", "2021-09-01-preview", true);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
             return message;
@@ -247,11 +245,11 @@ namespace Azure.Analytics.Purview.Share
             var request = message.Request;
             request.Method = RequestMethod.Post;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(_endpoint, false);
+            uri.Reset(_endpoint);
             uri.AppendPath("/receivedInvitations/", false);
             uri.AppendPath(receivedInvitationName, true);
             uri.AppendPath(":reject", false);
-            uri.AppendQuery("api-version", _apiVersion, true);
+            uri.AppendQuery("api-version", "2021-09-01-preview", true);
             request.Uri = uri;
             if (repeatabilityRequestId != null)
             {
@@ -269,7 +267,7 @@ namespace Azure.Analytics.Purview.Share
             var request = message.Request;
             request.Method = RequestMethod.Get;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(_endpoint, false);
+            uri.Reset(_endpoint);
             uri.AppendRawNextLink(nextLink, false);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
