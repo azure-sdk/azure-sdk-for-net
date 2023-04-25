660fcf363f92bbfabfb005a5e5a7a9b893ed7f6a
SDKAuto <sdkautomation@microsoft.com>
2023-04-25 08:02:13 +0000

    CodeGen from PR 23651 in Azure/azure-rest-api-specs Merge a1992a769485b9e91b4287bb5939689461612def into af3e299279d41ea879cab44db46663c2fac692ad

--- sdk/confidentialledger/Azure.ResourceManager.ConfidentialLedger/src/Generated/Models/ManagedCCFData.Serialization.cs
+++ sdk/confidentialledger/Azure.ResourceManager.ConfidentialLedger/src/Generated/Models/ManagedCCFData.Serialization.cs
@@ -13,16 +13,11 @@ using Azure.ResourceManager.Models;
 
 namespace Azure.ResourceManager.ConfidentialLedger
 {
-    public partial class ConfidentialLedgerData : IUtf8JsonSerializable
+    public partial class ManagedCCFData : IUtf8JsonSerializable
     {
         void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
         {
             writer.WriteStartObject();
-            if (Optional.IsDefined(RunningState))
-            {
-                writer.WritePropertyName("runningState"u8);
-                writer.WriteStringValue(RunningState.Value.ToString());
-            }
             if (Optional.IsDefined(Properties))
             {
                 writer.WritePropertyName("properties"u8);
@@ -44,14 +39,13 @@ namespace Azure.ResourceManager.ConfidentialLedger
             writer.WriteEndObject();
         }
 
-        internal static ConfidentialLedgerData DeserializeConfidentialLedgerData(JsonElement element)
+        internal static ManagedCCFData DeserializeManagedCCFData(JsonElement element)
         {
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
-            Optional<RunningState> runningState = default;
-            Optional<ConfidentialLedgerProperties> properties = default;
+            Optional<ManagedCCFProperties> properties = default;
             Optional<IDictionary<string, string>> tags = default;
             AzureLocation location = default;
             ResourceIdentifier id = default;
@@ -60,22 +54,13 @@ namespace Azure.ResourceManager.ConfidentialLedger
             Optional<SystemData> systemData = default;
             foreach (var property in element.EnumerateObject())
             {
-                if (property.NameEquals("runningState"u8))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        continue;
-                    }
-                    runningState = new RunningState(property.Value.GetString());
-                    continue;
-                }
                 if (property.NameEquals("properties"u8))
                 {
                     if (property.Value.ValueKind == JsonValueKind.Null)
                     {
                         continue;
                     }
-                    properties = ConfidentialLedgerProperties.DeserializeConfidentialLedgerProperties(property.Value);
+                    properties = ManagedCCFProperties.DeserializeManagedCCFProperties(property.Value);
                     continue;
                 }
                 if (property.NameEquals("tags"u8))
@@ -122,7 +107,7 @@ namespace Azure.ResourceManager.ConfidentialLedger
                     continue;
                 }
             }
-            return new ConfidentialLedgerData(id, name, type, systemData.Value, Optional.ToDictionary(tags), location, Optional.ToNullable(runningState), properties.Value);
+            return new ManagedCCFData(id, name, type, systemData.Value, Optional.ToDictionary(tags), location, properties.Value);
         }
     }
 }
