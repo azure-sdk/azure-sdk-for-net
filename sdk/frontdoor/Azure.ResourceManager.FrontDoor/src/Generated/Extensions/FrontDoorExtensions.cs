ae8d8f43c42844c7127e2664eb75074131704fa1
SDKAuto <sdkautomation@microsoft.com>
2023-07-03 07:23:32 +0000

    CodeGen from PR 24675 in Azure/azure-rest-api-specs Merge 920210cfdc433eaea5fecd8e07abc28667844b2a into fb32ae45a6f597e13d3c6f7675c86835c9ee1097

--- sdk/frontdoor/Azure.ResourceManager.FrontDoor/src/Generated/Extensions/FrontDoorExtensions.cs
+++ sdk/frontdoor/Azure.ResourceManager.FrontDoor/src/Generated/Extensions/FrontDoorExtensions.cs
@@ -349,6 +349,48 @@ namespace Azure.ResourceManager.FrontDoor
         }
 
         /// <summary>
+        /// Lists all of the protection policies within a subscription.
+        /// <list type="bullet">
+        /// <item>
+        /// <term>Request Path</term>
+        /// <description>/subscriptions/{subscriptionId}/providers/Microsoft.Network/frontDoorWebApplicationFirewallPolicies</description>
+        /// </item>
+        /// <item>
+        /// <term>Operation Id</term>
+        /// <description>Policies_ListBySubscription</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        /// <param name="subscriptionResource"> The <see cref="SubscriptionResource" /> instance the method will execute against. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns> An async collection of <see cref="FrontDoorWebApplicationFirewallPolicyResource" /> that may take multiple service requests to iterate over. </returns>
+        public static AsyncPageable<FrontDoorWebApplicationFirewallPolicyResource> GetFrontDoorWebApplicationFirewallPoliciesByFrontDoorWebApplicationFirewallPolicyAsync(this SubscriptionResource subscriptionResource, CancellationToken cancellationToken = default)
+        {
+            return GetSubscriptionResourceExtensionClient(subscriptionResource).GetFrontDoorWebApplicationFirewallPoliciesByFrontDoorWebApplicationFirewallPolicyAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Lists all of the protection policies within a subscription.
+        /// <list type="bullet">
+        /// <item>
+        /// <term>Request Path</term>
+        /// <description>/subscriptions/{subscriptionId}/providers/Microsoft.Network/frontDoorWebApplicationFirewallPolicies</description>
+        /// </item>
+        /// <item>
+        /// <term>Operation Id</term>
+        /// <description>Policies_ListBySubscription</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        /// <param name="subscriptionResource"> The <see cref="SubscriptionResource" /> instance the method will execute against. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns> A collection of <see cref="FrontDoorWebApplicationFirewallPolicyResource" /> that may take multiple service requests to iterate over. </returns>
+        public static Pageable<FrontDoorWebApplicationFirewallPolicyResource> GetFrontDoorWebApplicationFirewallPoliciesByFrontDoorWebApplicationFirewallPolicy(this SubscriptionResource subscriptionResource, CancellationToken cancellationToken = default)
+        {
+            return GetSubscriptionResourceExtensionClient(subscriptionResource).GetFrontDoorWebApplicationFirewallPoliciesByFrontDoorWebApplicationFirewallPolicy(cancellationToken);
+        }
+
+        /// <summary>
         /// Lists all available managed rule sets.
         /// <list type="bullet">
         /// <item>
