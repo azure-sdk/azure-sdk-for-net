6e3f655828c308408f1008bde7368a05a4126b7b
SDKAuto <sdkautomation@microsoft.com>
2023-08-22 02:04:40 +0000

    CodeGen from PR 25263 in Azure/azure-rest-api-specs Merge 3922268daf482b730c13dc65f4b58700fa08092b into bcfe02ef3ec205b5e52d927680d5c5f261c20a7a

--- sdk/hdinsight/Azure.ResourceManager.HDInsight/src/Generated/Models/ClusterListResult.cs
+++ sdk/hdinsight/Azure.ResourceManager.HDInsight/src/Generated/Models/ClusterListResult.cs
@@ -11,7 +11,7 @@ using Azure.ResourceManager.HDInsight;
 
 namespace Azure.ResourceManager.HDInsight.Models
 {
-    /// <summary> The List Cluster operation response. </summary>
+    /// <summary> The list cluster operation response. </summary>
     internal partial class ClusterListResult
     {
         /// <summary> Initializes a new instance of ClusterListResult. </summary>
@@ -21,7 +21,7 @@ namespace Azure.ResourceManager.HDInsight.Models
         }
 
         /// <summary> Initializes a new instance of ClusterListResult. </summary>
-        /// <param name="value"> The list of Clusters. </param>
+        /// <param name="value"> The list of clusters. </param>
         /// <param name="nextLink"> The link (url) to the next page of results. </param>
         internal ClusterListResult(IReadOnlyList<HDInsightClusterData> value, string nextLink)
         {
@@ -29,7 +29,7 @@ namespace Azure.ResourceManager.HDInsight.Models
             NextLink = nextLink;
         }
 
-        /// <summary> The list of Clusters. </summary>
+        /// <summary> The list of clusters. </summary>
         public IReadOnlyList<HDInsightClusterData> Value { get; }
         /// <summary> The link (url) to the next page of results. </summary>
         public string NextLink { get; }
