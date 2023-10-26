eb52113ca085caf9fb5bd6cd425a011bb0645a06
SDKAuto <sdkautomation@microsoft.com>
2023-10-26 23:32:14 +0000

    CodeGen from PR 26312 in Azure/azure-rest-api-specs Merge f8f76a5ca59a5aa89d6a92636006aa46e48cf132 into 1e410d42c1932737d545fcf91a38594d04ca9c22

--- sdk/compute/Azure.ResourceManager.Compute/samples/Generated/Samples/Sample_DedicatedHostResource.cs
+++ sdk/compute/Azure.ResourceManager.Compute/samples/Generated/Samples/Sample_DedicatedHostResource.cs
@@ -252,6 +252,34 @@ namespace Azure.ResourceManager.Compute.Samples
             Console.WriteLine($"Succeeded");
         }
 
+        // Redeploy Dedicated Host.
+        [NUnit.Framework.Test]
+        [NUnit.Framework.Ignore("Only verifying that the sample builds")]
+        public async Task Redeploy_RedeployDedicatedHost()
+        {
+            // Generated from example definition: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-07-01/examples/dedicatedHostExamples/DedicatedHost_Redeploy.json
+            // this example is just showing the usage of "DedicatedHosts_Redeploy" operation, for the dependent resources, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this DedicatedHostResource created on azure
+            // for more information of creating DedicatedHostResource, please refer to the document of DedicatedHostResource
+            string subscriptionId = "{subscription-id}";
+            string resourceGroupName = "myResourceGroup";
+            string hostGroupName = "myDedicatedHostGroup";
+            string hostName = "myHost";
+            ResourceIdentifier dedicatedHostResourceId = DedicatedHostResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, hostGroupName, hostName);
+            DedicatedHostResource dedicatedHost = client.GetDedicatedHostResource(dedicatedHostResourceId);
+
+            // invoke the operation
+            await dedicatedHost.RedeployAsync(WaitUntil.Completed);
+
+            Console.WriteLine($"Succeeded");
+        }
+
         // Get Available Dedicated Host Sizes.
         [NUnit.Framework.Test]
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
