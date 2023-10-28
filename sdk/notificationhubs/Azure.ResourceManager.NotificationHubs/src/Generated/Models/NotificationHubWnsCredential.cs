7c4aab6704aa7bd3d3c7d8c15b7be4621720a492
SDKAuto <sdkautomation@microsoft.com>
2023-10-28 00:17:08 +0000

    CodeGen from PR 26443 in Azure/azure-rest-api-specs Merge 8a1deb534e31ac60d5e60abb214ecea0de3fac30 into 58e92dd03733bc175e6a9540f4bc53703b57fcc9

--- sdk/notificationhubs/Azure.ResourceManager.NotificationHubs/src/Generated/Models/NotificationHubWnsCredential.cs
+++ sdk/notificationhubs/Azure.ResourceManager.NotificationHubs/src/Generated/Models/NotificationHubWnsCredential.cs
@@ -6,33 +6,24 @@
 #nullable disable
 
 using System;
+using Azure.Core;
 
 namespace Azure.ResourceManager.NotificationHubs.Models
 {
     /// <summary> Description of a NotificationHub WnsCredential. </summary>
-    public partial class NotificationHubWnsCredential
+    internal partial class NotificationHubWnsCredential
     {
         /// <summary> Initializes a new instance of NotificationHubWnsCredential. </summary>
-        public NotificationHubWnsCredential()
+        /// <param name="properties"> Description of a NotificationHub WnsCredential. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="properties"/> is null. </exception>
+        public NotificationHubWnsCredential(WnsCredentialProperties properties)
         {
+            Argument.AssertNotNull(properties, nameof(properties));
+
+            Properties = properties;
         }
 
-        /// <summary> Initializes a new instance of NotificationHubWnsCredential. </summary>
-        /// <param name="packageSid"> The package ID for this credential. </param>
-        /// <param name="secretKey"> The secret key. </param>
-        /// <param name="windowsLiveEndpoint"> The Windows Live endpoint. </param>
-        internal NotificationHubWnsCredential(string packageSid, string secretKey, Uri windowsLiveEndpoint)
-        {
-            PackageSid = packageSid;
-            SecretKey = secretKey;
-            WindowsLiveEndpoint = windowsLiveEndpoint;
-        }
-
-        /// <summary> The package ID for this credential. </summary>
-        public string PackageSid { get; set; }
-        /// <summary> The secret key. </summary>
-        public string SecretKey { get; set; }
-        /// <summary> The Windows Live endpoint. </summary>
-        public Uri WindowsLiveEndpoint { get; set; }
+        /// <summary> Description of a NotificationHub WnsCredential. </summary>
+        public WnsCredentialProperties Properties { get; set; }
     }
 }
