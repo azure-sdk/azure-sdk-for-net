775cb37fa4bef8d49d5ae2a9e1d50255ddc43057
SDKAuto <sdkautomation@microsoft.com>
2023-11-15 04:01:06 +0000

    CodeGen from PR 26699 in Azure/azure-rest-api-specs java, simplify openai client.tsp (#26699)

--- sdk/openai/Azure.AI.OpenAI/src/Generated/ImageGenerationOptions.Serialization.cs
+++ sdk/openai/Azure.AI.OpenAI/src/Generated/ImageGenerationOptions.Serialization.cs
@@ -6,6 +6,7 @@
 #nullable disable
 
 using System.Text.Json;
+using Azure;
 using Azure.Core;
 
 namespace Azure.AI.OpenAI
@@ -38,6 +39,68 @@ namespace Azure.AI.OpenAI
                 writer.WriteStringValue(User);
             }
             writer.WriteEndObject();
+        }
+
+        internal static ImageGenerationOptions DeserializeImageGenerationOptions(JsonElement element)
+        {
+            if (element.ValueKind == JsonValueKind.Null)
+            {
+                return null;
+            }
+            string prompt = default;
+            Optional<int> n = default;
+            Optional<ImageSize> size = default;
+            Optional<ImageGenerationResponseFormat> responseFormat = default;
+            Optional<string> user = default;
+            foreach (var property in element.EnumerateObject())
+            {
+                if (property.NameEquals("prompt"u8))
+                {
+                    prompt = property.Value.GetString();
+                    continue;
+                }
+                if (property.NameEquals("n"u8))
+                {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        continue;
+                    }
+                    n = property.Value.GetInt32();
+                    continue;
+                }
+                if (property.NameEquals("size"u8))
+                {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        continue;
+                    }
+                    size = new ImageSize(property.Value.GetString());
+                    continue;
+                }
+                if (property.NameEquals("response_format"u8))
+                {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        continue;
+                    }
+                    responseFormat = new ImageGenerationResponseFormat(property.Value.GetString());
+                    continue;
+                }
+                if (property.NameEquals("user"u8))
+                {
+                    user = property.Value.GetString();
+                    continue;
+                }
+            }
+            return new ImageGenerationOptions(prompt, Optional.ToNullable(n), Optional.ToNullable(size), Optional.ToNullable(responseFormat), user.Value);
+        }
+
+        /// <summary> Deserializes the model from a raw response. </summary>
+        /// <param name="response"> The response to deserialize the model from. </param>
+        internal static ImageGenerationOptions FromResponse(Response response)
+        {
+            using var document = JsonDocument.Parse(response.Content);
+            return DeserializeImageGenerationOptions(document.RootElement);
         }
 
         /// <summary> Convert into a Utf8JsonRequestContent. </summary>
