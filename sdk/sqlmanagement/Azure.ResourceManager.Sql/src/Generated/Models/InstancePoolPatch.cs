f5f1aa58a12acef1f9e939ab42077cb1149b9b49
SDKAuto <sdkautomation@microsoft.com>
2023-11-22 08:21:53 +0000

    CodeGen from PR 26813 in Azure/azure-rest-api-specs Merge 29d9aebfe5b7da466b8a054b46b6695882bbc797 into 7ae8d80917d262746790f86c98c5191bd0ce8eff

--- sdk/sqlmanagement/Azure.ResourceManager.Sql/src/Generated/Models/InstancePoolPatch.cs
+++ sdk/sqlmanagement/Azure.ResourceManager.Sql/src/Generated/Models/InstancePoolPatch.cs
@@ -19,7 +19,19 @@ namespace Azure.ResourceManager.Sql.Models
             Tags = new ChangeTrackingDictionary<string, string>();
         }
 
+        /// <summary> The name and tier of the SKU. </summary>
+        public SqlSku Sku { get; set; }
         /// <summary> Resource tags. </summary>
         public IDictionary<string, string> Tags { get; }
+        /// <summary> Resource ID of the subnet to place this instance pool in. </summary>
+        public ResourceIdentifier SubnetId { get; set; }
+        /// <summary> Count of vCores belonging to this instance pool. </summary>
+        public int? VCores { get; set; }
+        /// <summary> The license type. Possible values are 'LicenseIncluded' (price for SQL license is included) and 'BasePrice' (without SQL license price). </summary>
+        public InstancePoolLicenseType? LicenseType { get; set; }
+        /// <summary> The Dns Zone that the managed instance pool is in. </summary>
+        public string DnsZone { get; }
+        /// <summary> Specifies maintenance configuration id to apply to this managed instance. </summary>
+        public ResourceIdentifier MaintenanceConfigurationId { get; set; }
     }
 }
