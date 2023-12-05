97b143c923e36dbb2c0e3240d22290af1a1a72e9
SDKAuto <sdkautomation@microsoft.com>
2023-12-05 04:32:33 +0000

    CodeGen from PR 26846 in Azure/azure-rest-api-specs Merge f85dddf283968237c34e2df934f0d830eddb9270 into f0614ca75803b74019ea1208bdda48967ef44fb6

--- sdk/datafactory/Azure.ResourceManager.DataFactory/src/Generated/Models/WebActivity.Serialization.cs
+++ sdk/datafactory/Azure.ResourceManager.DataFactory/src/Generated/Models/WebActivity.Serialization.cs
@@ -93,6 +93,16 @@ namespace Azure.ResourceManager.DataFactory.Models
                 writer.WritePropertyName("disableCertValidation"u8);
                 writer.WriteBooleanValue(DisableCertValidation.Value);
             }
+            if (Optional.IsDefined(HttpRequestTimeout))
+            {
+                writer.WritePropertyName("httpRequestTimeout"u8);
+                JsonSerializer.Serialize(writer, HttpRequestTimeout);
+            }
+            if (Optional.IsDefined(TurnOffAsync))
+            {
+                writer.WritePropertyName("turnOffAsync"u8);
+                writer.WriteBooleanValue(TurnOffAsync.Value);
+            }
             if (Optional.IsCollectionDefined(Datasets))
             {
                 writer.WritePropertyName("datasets"u8);
@@ -151,10 +161,12 @@ namespace Azure.ResourceManager.DataFactory.Models
             Optional<IList<PipelineActivityUserProperty>> userProperties = default;
             WebActivityMethod method = default;
             DataFactoryElement<string> url = default;
-            Optional<DataFactoryElement<string>> headers = default;
+            Optional<DataFactoryElement<IDictionary<string, string>>> headers = default;
             Optional<DataFactoryElement<string>> body = default;
             Optional<WebActivityAuthentication> authentication = default;
             Optional<bool> disableCertValidation = default;
+            Optional<DataFactoryElement<string>> httpRequestTimeout = default;
+            Optional<bool> turnOffAsync = default;
             Optional<IList<DatasetReference>> datasets = default;
             Optional<IList<DataFactoryLinkedServiceReference>> linkedServices = default;
             Optional<IntegrationRuntimeReference> connectVia = default;
@@ -266,7 +278,7 @@ namespace Azure.ResourceManager.DataFactory.Models
                             {
                                 continue;
                             }
-                            headers = JsonSerializer.Deserialize<DataFactoryElement<string>>(property0.Value.GetRawText());
+                            headers = JsonSerializer.Deserialize<DataFactoryElement<IDictionary<string, string>>>(property0.Value.GetRawText());
                             continue;
                         }
                         if (property0.NameEquals("body"u8))
@@ -294,6 +306,24 @@ namespace Azure.ResourceManager.DataFactory.Models
                                 continue;
                             }
                             disableCertValidation = property0.Value.GetBoolean();
+                            continue;
+                        }
+                        if (property0.NameEquals("httpRequestTimeout"u8))
+                        {
+                            if (property0.Value.ValueKind == JsonValueKind.Null)
+                            {
+                                continue;
+                            }
+                            httpRequestTimeout = JsonSerializer.Deserialize<DataFactoryElement<string>>(property0.Value.GetRawText());
+                            continue;
+                        }
+                        if (property0.NameEquals("turnOffAsync"u8))
+                        {
+                            if (property0.Value.ValueKind == JsonValueKind.Null)
+                            {
+                                continue;
+                            }
+                            turnOffAsync = property0.Value.GetBoolean();
                             continue;
                         }
                         if (property0.NameEquals("datasets"u8))
@@ -339,7 +369,7 @@ namespace Azure.ResourceManager.DataFactory.Models
                 additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
             }
             additionalProperties = additionalPropertiesDictionary;
-            return new WebActivity(name, type, description.Value, Optional.ToNullable(state), Optional.ToNullable(onInactiveMarkAs), Optional.ToList(dependsOn), Optional.ToList(userProperties), additionalProperties, linkedServiceName, policy.Value, method, url, headers.Value, body.Value, authentication.Value, Optional.ToNullable(disableCertValidation), Optional.ToList(datasets), Optional.ToList(linkedServices), connectVia.Value);
+            return new WebActivity(name, type, description.Value, Optional.ToNullable(state), Optional.ToNullable(onInactiveMarkAs), Optional.ToList(dependsOn), Optional.ToList(userProperties), additionalProperties, linkedServiceName, policy.Value, method, url, headers.Value, body.Value, authentication.Value, Optional.ToNullable(disableCertValidation), httpRequestTimeout.Value, Optional.ToNullable(turnOffAsync), Optional.ToList(datasets), Optional.ToList(linkedServices), connectVia.Value);
         }
     }
 }
