e2d5bc9d90c27e3c968b3a9fb99a52ac11893349
SDKAuto <sdkautomation@microsoft.com>
2023-12-15 07:04:49 +0000

    CodeGen from PR 27100 in Azure/azure-rest-api-specs Merge 1990881d3f72fa2eec523997e8596b4e363d70ef into 91f4459f6d2221c15451e21e32865e7194eb4658

--- sdk/websites/Azure.ResourceManager.AppService/src/Generated/Models/AppServiceCertificateEmail.Serialization.cs
+++ sdk/websites/Azure.ResourceManager.AppService/src/Generated/Models/AppServiceCertificateEmail.Serialization.cs
@@ -8,108 +8,37 @@
 using System;
 using System.Text.Json;
 using Azure.Core;
-using Azure.ResourceManager.Models;
 
 namespace Azure.ResourceManager.AppService.Models
 {
-    public partial class AppServiceCertificateEmail : IUtf8JsonSerializable
+    public partial class AppServiceCertificateEmail
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
-        {
-            writer.WriteStartObject();
-            if (Optional.IsDefined(Kind))
-            {
-                writer.WritePropertyName("kind"u8);
-                writer.WriteStringValue(Kind);
-            }
-            writer.WritePropertyName("properties"u8);
-            writer.WriteStartObject();
-            if (Optional.IsDefined(EmailId))
-            {
-                writer.WritePropertyName("emailId"u8);
-                writer.WriteStringValue(EmailId);
-            }
-            if (Optional.IsDefined(TimeStamp))
-            {
-                writer.WritePropertyName("timeStamp"u8);
-                writer.WriteStringValue(TimeStamp.Value, "O");
-            }
-            writer.WriteEndObject();
-            writer.WriteEndObject();
-        }
-
         internal static AppServiceCertificateEmail DeserializeAppServiceCertificateEmail(JsonElement element)
         {
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
-            Optional<string> kind = default;
-            ResourceIdentifier id = default;
-            string name = default;
-            ResourceType type = default;
-            Optional<SystemData> systemData = default;
             Optional<string> emailId = default;
             Optional<DateTimeOffset> timeStamp = default;
             foreach (var property in element.EnumerateObject())
             {
-                if (property.NameEquals("kind"u8))
+                if (property.NameEquals("emailId"u8))
                 {
-                    kind = property.Value.GetString();
+                    emailId = property.Value.GetString();
                     continue;
                 }
-                if (property.NameEquals("id"u8))
-                {
-                    id = new ResourceIdentifier(property.Value.GetString());
-                    continue;
-                }
-                if (property.NameEquals("name"u8))
-                {
-                    name = property.Value.GetString();
-                    continue;
-                }
-                if (property.NameEquals("type"u8))
-                {
-                    type = new ResourceType(property.Value.GetString());
-                    continue;
-                }
-                if (property.NameEquals("systemData"u8))
+                if (property.NameEquals("timeStamp"u8))
                 {
                     if (property.Value.ValueKind == JsonValueKind.Null)
                     {
                         continue;
                     }
-                    systemData = JsonSerializer.Deserialize<SystemData>(property.Value.GetRawText());
-                    continue;
-                }
-                if (property.NameEquals("properties"u8))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        property.ThrowNonNullablePropertyIsNull();
-                        continue;
-                    }
-                    foreach (var property0 in property.Value.EnumerateObject())
-                    {
-                        if (property0.NameEquals("emailId"u8))
-                        {
-                            emailId = property0.Value.GetString();
-                            continue;
-                        }
-                        if (property0.NameEquals("timeStamp"u8))
-                        {
-                            if (property0.Value.ValueKind == JsonValueKind.Null)
-                            {
-                                continue;
-                            }
-                            timeStamp = property0.Value.GetDateTimeOffset("O");
-                            continue;
-                        }
-                    }
+                    timeStamp = property.Value.GetDateTimeOffset("O");
                     continue;
                 }
             }
-            return new AppServiceCertificateEmail(id, name, type, systemData.Value, emailId.Value, Optional.ToNullable(timeStamp), kind.Value);
+            return new AppServiceCertificateEmail(emailId.Value, Optional.ToNullable(timeStamp));
         }
     }
 }
