93105eef9e7040ff901c67680c9d9a10f02884af
SDKAuto <sdkautomation@microsoft.com>
2024-02-17 02:11:52 +0000

    CodeGen from PR 27813 in Azure/azure-rest-api-specs Merge 2d3b2f15095531af0fd67ae8287db6d04c704d8d into dc15daf7fe1aecbac15cdc0f98c8579441897bda

--- sdk/databoxedge/Azure.ResourceManager.DataBoxEdge/src/Generated/DataBoxEdgeStorageAccountResource.cs
+++ sdk/databoxedge/Azure.ResourceManager.DataBoxEdge/src/Generated/DataBoxEdgeStorageAccountResource.cs
@@ -110,7 +110,7 @@ namespace Azure.ResourceManager.DataBoxEdge
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
-        /// <description>2022-03-01</description>
+        /// <description>2023-12-01</description>
         /// </item>
         /// <item>
         /// <term>Resource</term>
@@ -141,7 +141,7 @@ namespace Azure.ResourceManager.DataBoxEdge
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
-        /// <description>2022-03-01</description>
+        /// <description>2023-12-01</description>
         /// </item>
         /// <item>
         /// <term>Resource</term>
@@ -172,7 +172,7 @@ namespace Azure.ResourceManager.DataBoxEdge
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
-        /// <description>2022-03-01</description>
+        /// <description>2023-12-01</description>
         /// </item>
         /// <item>
         /// <term>Resource</term>
@@ -212,7 +212,7 @@ namespace Azure.ResourceManager.DataBoxEdge
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
-        /// <description>2022-03-01</description>
+        /// <description>2023-12-01</description>
         /// </item>
         /// <item>
         /// <term>Resource</term>
@@ -252,7 +252,7 @@ namespace Azure.ResourceManager.DataBoxEdge
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
-        /// <description>2022-03-01</description>
+        /// <description>2023-12-01</description>
         /// </item>
         /// <item>
         /// <term>Resource</term>
@@ -294,7 +294,7 @@ namespace Azure.ResourceManager.DataBoxEdge
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
-        /// <description>2022-03-01</description>
+        /// <description>2023-12-01</description>
         /// </item>
         /// <item>
         /// <term>Resource</term>
@@ -336,7 +336,7 @@ namespace Azure.ResourceManager.DataBoxEdge
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
-        /// <description>2022-03-01</description>
+        /// <description>2023-12-01</description>
         /// </item>
         /// <item>
         /// <term>Resource</term>
@@ -382,7 +382,7 @@ namespace Azure.ResourceManager.DataBoxEdge
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
-        /// <description>2022-03-01</description>
+        /// <description>2023-12-01</description>
         /// </item>
         /// <item>
         /// <term>Resource</term>
