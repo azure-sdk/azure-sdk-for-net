cd64f2ddb8a34481a4252843556f91ccf3967783
SDKAuto <sdkautomation@microsoft.com>
2024-05-14 04:51:56 +0000

    CodeGen from PR 3349 in test-repo-billy/azure-rest-api-specs Merge 935e8d5c4e3382f90879a6b914dabfdeb9b5a448 into 9789b00f225d650cdf2c6623967095ea21614520

--- sdk/textanalytics/Azure.AI.TextAnalytics/src/Generated/Models/EntitiesResult.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/Generated/Models/EntitiesResult.cs
@@ -11,20 +11,22 @@ using System.Linq;
 
 namespace Azure.AI.TextAnalytics.Models
 {
-    /// <summary> The EntitiesResult. </summary>
-    internal partial class EntitiesResult : PreBuiltResult
+    /// <summary> Contains the entity recognition task result. </summary>
+    internal partial class EntitiesResult
     {
         /// <summary> Initializes a new instance of <see cref="EntitiesResult"/>. </summary>
         /// <param name="errors"> Errors by document id. </param>
         /// <param name="modelVersion"> This field indicates which model is used for scoring. </param>
         /// <param name="documents"> Response by document. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="errors"/>, <paramref name="modelVersion"/> or <paramref name="documents"/> is null. </exception>
-        public EntitiesResult(IEnumerable<DocumentError> errors, string modelVersion, IEnumerable<EntitiesResultDocumentsItem> documents) : base(errors, modelVersion)
+        internal EntitiesResult(IEnumerable<DocumentError> errors, string modelVersion, IEnumerable<EntitiesDocumentResult> documents)
         {
             Argument.AssertNotNull(errors, nameof(errors));
             Argument.AssertNotNull(modelVersion, nameof(modelVersion));
             Argument.AssertNotNull(documents, nameof(documents));
 
+            Errors = errors.ToList();
+            ModelVersion = modelVersion;
             Documents = documents.ToList();
         }
 
@@ -33,12 +35,21 @@ namespace Azure.AI.TextAnalytics.Models
         /// <param name="statistics"> if showStats=true was specified in the request this field will contain information about the request payload. </param>
         /// <param name="modelVersion"> This field indicates which model is used for scoring. </param>
         /// <param name="documents"> Response by document. </param>
-        internal EntitiesResult(IList<DocumentError> errors, TextDocumentBatchStatistics statistics, string modelVersion, IList<EntitiesResultDocumentsItem> documents) : base(errors, statistics, modelVersion)
+        internal EntitiesResult(IReadOnlyList<DocumentError> errors, TextDocumentBatchStatistics statistics, string modelVersion, IReadOnlyList<EntitiesDocumentResult> documents)
         {
+            Errors = errors;
+            Statistics = statistics;
+            ModelVersion = modelVersion;
             Documents = documents;
         }
 
+        /// <summary> Errors by document id. </summary>
+        public IReadOnlyList<DocumentError> Errors { get; }
+        /// <summary> if showStats=true was specified in the request this field will contain information about the request payload. </summary>
+        public TextDocumentBatchStatistics Statistics { get; }
+        /// <summary> This field indicates which model is used for scoring. </summary>
+        public string ModelVersion { get; }
         /// <summary> Response by document. </summary>
-        public IList<EntitiesResultDocumentsItem> Documents { get; }
+        public IReadOnlyList<EntitiesDocumentResult> Documents { get; }
     }
 }
