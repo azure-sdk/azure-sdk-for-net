5f9fbc72c18b68fa52cec9859c12ab06896669be
SDKAuto <sdkautomation@microsoft.com>
2024-06-07 18:17:56 +0000

    CodeGen from PR 29357 in Azure/azure-rest-api-specs Merge 39b3a22bbb721afb9bb334fbdcf93964d7614bd7 into c37ec243f1ed3abf88fb923eeee02d93d646a7c7

--- sdk/network/Microsoft.Azure.Management.Network/src/Generated/Models/VirtualNetworkTap.cs
+++ sdk/network/Microsoft.Azure.Management.Network/src/Generated/Models/VirtualNetworkTap.cs
@@ -125,5 +125,28 @@ namespace Microsoft.Azure.Management.Network.Models
         [JsonProperty(PropertyName = "etag")]
         public string Etag { get; private set; }
 
+        /// <summary>
+        /// Validate the object.
+        /// </summary>
+        /// <exception cref="ValidationException">
+        /// Thrown if validation fails
+        /// </exception>
+        public virtual void Validate()
+        {
+            if (NetworkInterfaceTapConfigurations != null)
+            {
+                foreach (var element in NetworkInterfaceTapConfigurations)
+                {
+                    if (element != null)
+                    {
+                        element.Validate();
+                    }
+                }
+            }
+            if (DestinationNetworkInterfaceIPConfiguration != null)
+            {
+                DestinationNetworkInterfaceIPConfiguration.Validate();
+            }
+        }
     }
 }
