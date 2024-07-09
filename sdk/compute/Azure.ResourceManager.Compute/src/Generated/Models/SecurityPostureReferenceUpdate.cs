757f9bc7183d7c545296422cc15a020b30a00415
SDKAuto <sdkautomation@microsoft.com>
2024-07-09 18:38:12 +0000

    CodeGen from PR 29402 in Azure/azure-rest-api-specs Merge f5aad737b349e0a40094eefa3e384b3754fda3b9 into 125c6cf230b0ac8a83838dd4c4d55fd6bdca839a

--- sdk/compute/Azure.ResourceManager.Compute/src/Generated/Models/SecurityPostureReferenceUpdate.cs
+++ sdk/compute/Azure.ResourceManager.Compute/src/Generated/Models/SecurityPostureReferenceUpdate.cs
@@ -7,12 +7,11 @@
 
 using System;
 using System.Collections.Generic;
-using Azure.ResourceManager.Resources.Models;
 
 namespace Azure.ResourceManager.Compute.Models
 {
-    /// <summary> The ResourceSharingProfile. </summary>
-    internal partial class ResourceSharingProfile
+    /// <summary> Specifies the security posture to be used in the scale set. Minimum api-version: 2023-03-01. </summary>
+    public partial class SecurityPostureReferenceUpdate
     {
         /// <summary>
         /// Keeps track of any properties unknown to the library.
@@ -46,22 +45,30 @@ namespace Azure.ResourceManager.Compute.Models
         /// </summary>
         private IDictionary<string, BinaryData> _serializedAdditionalRawData;
 
-        /// <summary> Initializes a new instance of <see cref="ResourceSharingProfile"/>. </summary>
-        public ResourceSharingProfile()
+        /// <summary> Initializes a new instance of <see cref="SecurityPostureReferenceUpdate"/>. </summary>
+        public SecurityPostureReferenceUpdate()
         {
-            SubscriptionIds = new ChangeTrackingList<WritableSubResource>();
+            ExcludeExtensions = new ChangeTrackingList<string>();
         }
 
-        /// <summary> Initializes a new instance of <see cref="ResourceSharingProfile"/>. </summary>
-        /// <param name="subscriptionIds"> Specifies an array of subscription resource IDs that capacity reservation group is shared with. **Note:** Minimum api-version: 2023-09-01. Please refer to https://aka.ms/computereservationsharing for more details. </param>
+        /// <summary> Initializes a new instance of <see cref="SecurityPostureReferenceUpdate"/>. </summary>
+        /// <param name="id"> The security posture reference id in the form of /CommunityGalleries/{communityGalleryName}/securityPostures/{securityPostureName}/versions/{major.minor.patch}|latest. </param>
+        /// <param name="excludeExtensions"> The list of virtual machine extension names to exclude when applying the security posture. </param>
+        /// <param name="isOverridable"> Whether the security posture can be overridden by the user. </param>
         /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
-        internal ResourceSharingProfile(IList<WritableSubResource> subscriptionIds, IDictionary<string, BinaryData> serializedAdditionalRawData)
+        internal SecurityPostureReferenceUpdate(string id, IList<string> excludeExtensions, bool? isOverridable, IDictionary<string, BinaryData> serializedAdditionalRawData)
         {
-            SubscriptionIds = subscriptionIds;
+            Id = id;
+            ExcludeExtensions = excludeExtensions;
+            IsOverridable = isOverridable;
             _serializedAdditionalRawData = serializedAdditionalRawData;
         }
 
-        /// <summary> Specifies an array of subscription resource IDs that capacity reservation group is shared with. **Note:** Minimum api-version: 2023-09-01. Please refer to https://aka.ms/computereservationsharing for more details. </summary>
-        public IList<WritableSubResource> SubscriptionIds { get; }
+        /// <summary> The security posture reference id in the form of /CommunityGalleries/{communityGalleryName}/securityPostures/{securityPostureName}/versions/{major.minor.patch}|latest. </summary>
+        public string Id { get; set; }
+        /// <summary> The list of virtual machine extension names to exclude when applying the security posture. </summary>
+        public IList<string> ExcludeExtensions { get; }
+        /// <summary> Whether the security posture can be overridden by the user. </summary>
+        public bool? IsOverridable { get; set; }
     }
 }
