aecc81d949566db9a55940a356f95a2266ce5f01
SDKAuto <sdkautomation@microsoft.com>
2024-09-04 11:46:36 +0000

    CodeGen from PR 30422 in Azure/azure-rest-api-specs Merge f6075a00e5fa951cc0213d2a6b1776e141da714f into 13db45a1d86d193344bf3d461e0e176f6bd37029

--- sdk/eventgrid/Azure.Messaging.EventGrid.SystemEvents/src/Generated/AcsSmsReceivedEventData.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid.SystemEvents/src/Generated/AcsSmsReceivedEventData.cs
@@ -14,10 +14,8 @@ namespace Azure.Messaging.EventGrid.SystemEvents
     public partial class AcsSmsReceivedEventData : AcsSmsEventBaseProperties
     {
         /// <summary> Initializes a new instance of <see cref="AcsSmsReceivedEventData"/>. </summary>
-        /// <param name="receivedTimestamp"> The time at which the SMS was received. </param>
-        internal AcsSmsReceivedEventData(DateTimeOffset receivedTimestamp)
+        internal AcsSmsReceivedEventData()
         {
-            ReceivedTimestamp = receivedTimestamp;
         }
 
         /// <summary> Initializes a new instance of <see cref="AcsSmsReceivedEventData"/>. </summary>
@@ -27,20 +25,15 @@ namespace Azure.Messaging.EventGrid.SystemEvents
         /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
         /// <param name="message"> The SMS content. </param>
         /// <param name="receivedTimestamp"> The time at which the SMS was received. </param>
-        internal AcsSmsReceivedEventData(string messageId, string @from, string to, IDictionary<string, BinaryData> serializedAdditionalRawData, string message, DateTimeOffset receivedTimestamp) : base(messageId, @from, to, serializedAdditionalRawData)
+        internal AcsSmsReceivedEventData(string messageId, string @from, string to, IDictionary<string, BinaryData> serializedAdditionalRawData, string message, DateTimeOffset? receivedTimestamp) : base(messageId, @from, to, serializedAdditionalRawData)
         {
             Message = message;
             ReceivedTimestamp = receivedTimestamp;
         }
 
-        /// <summary> Initializes a new instance of <see cref="AcsSmsReceivedEventData"/> for deserialization. </summary>
-        internal AcsSmsReceivedEventData()
-        {
-        }
-
         /// <summary> The SMS content. </summary>
         public string Message { get; }
         /// <summary> The time at which the SMS was received. </summary>
-        public DateTimeOffset ReceivedTimestamp { get; }
+        public DateTimeOffset? ReceivedTimestamp { get; }
     }
 }
