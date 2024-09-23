bfbea029af46985f0381c5909c9bb2470e4501e6
SDKAuto <sdkautomation@microsoft.com>
2024-09-23 09:10:57 +0000

    CodeGen from PR 3504 in test-repo-billy/azure-rest-api-specs Merge f515d54057fda70c4f8baad667817b7c8b55c265 into 02e500856ce974e6873e205ffdb6523ffdc60bb7

--- sdk/authorization/Azure.ResourceManager.Authorization/samples/Generated/Samples/Sample_TenantResourceExtensions.cs
+++ sdk/authorization/Azure.ResourceManager.Authorization/samples/Generated/Samples/Sample_TenantResourceExtensions.cs
@@ -9,6 +9,7 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
+using Azure.ResourceManager.Authorization.Models;
 
 namespace Azure.ResourceManager.Authorization.Samples
 {
@@ -33,6 +34,32 @@ namespace Azure.ResourceManager.Authorization.Samples
 
             // invoke the operation
             await tenantResource.ElevateAccessGlobalAdministratorAsync();
+
+            Console.WriteLine($"Succeeded");
+        }
+
+        // List provider operations metadata for all resource providers
+        [NUnit.Framework.Test]
+        [NUnit.Framework.Ignore("Only verifying that the sample builds")]
+        public async Task GetAllProviderOperationsMetadata_ListProviderOperationsMetadataForAllResourceProviders()
+        {
+            // Generated from example definition: specification/authorization/resource-manager/Microsoft.Authorization/stable/2022-04-01/examples/GetAllProviderOperations.json
+            // this example is just showing the usage of "ProviderOperationsMetadata_List" operation, for the dependent resources, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this TenantResource created on azure
+            // for more information of creating TenantResource, please refer to the document of TenantResource
+            var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+
+            // invoke the operation and iterate over the result
+            await foreach (AuthorizationProviderOperationsMetadata item in tenantResource.GetAllProviderOperationsMetadataAsync())
+            {
+                Console.WriteLine($"Succeeded: {item}");
+            }
 
             Console.WriteLine($"Succeeded");
         }
