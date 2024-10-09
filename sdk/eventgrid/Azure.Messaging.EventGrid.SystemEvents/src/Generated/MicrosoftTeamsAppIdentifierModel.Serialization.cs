0960b1ed5c91b05efab83c926fdcb0397ac49fe0
SDKAuto <sdkautomation@microsoft.com>
2024-10-09 07:49:51 +0000

    CodeGen from PR 30578 in Azure/azure-rest-api-specs Merge 9f730fd52e48c9e604f212e0e69481df27c1fa99 into 63c41aa20e38fe6d2ddd1a367b4fe57e8b601c34

--- sdk/eventgrid/Azure.Messaging.EventGrid.SystemEvents/src/Generated/MicrosoftTeamsAppIdentifierModel.Serialization.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid.SystemEvents/src/Generated/MicrosoftTeamsAppIdentifierModel.Serialization.cs
@@ -28,8 +28,11 @@ namespace Azure.Messaging.EventGrid.SystemEvents
             writer.WriteStartObject();
             writer.WritePropertyName("appId"u8);
             writer.WriteStringValue(AppId);
-            writer.WritePropertyName("cloud"u8);
-            writer.WriteStringValue(Cloud.ToString());
+            if (Optional.IsDefined(Cloud))
+            {
+                writer.WritePropertyName("cloud"u8);
+                writer.WriteStringValue(Cloud.Value.ToString());
+            }
             if (options.Format != "W" && _serializedAdditionalRawData != null)
             {
                 foreach (var item in _serializedAdditionalRawData)
@@ -69,7 +72,7 @@ namespace Azure.Messaging.EventGrid.SystemEvents
                 return null;
             }
             string appId = default;
-            CommunicationCloudEnvironmentModel cloud = default;
+            CommunicationCloudEnvironmentModel? cloud = default;
             IDictionary<string, BinaryData> serializedAdditionalRawData = default;
             Dictionary<string, BinaryData> rawDataDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
@@ -81,6 +84,10 @@ namespace Azure.Messaging.EventGrid.SystemEvents
                 }
                 if (property.NameEquals("cloud"u8))
                 {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        continue;
+                    }
                     cloud = new CommunicationCloudEnvironmentModel(property.Value.GetString());
                     continue;
                 }
