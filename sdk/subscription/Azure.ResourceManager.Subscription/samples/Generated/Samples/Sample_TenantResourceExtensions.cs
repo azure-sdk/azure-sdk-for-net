7423ac6ba752c6aa3cd19f2bbf98c516ac818752
SDKAuto <sdkautomation@microsoft.com>
2024-10-14 20:09:10 +0000

    CodeGen from PR 30929 in Azure/azure-rest-api-specs Merge 07f8dc4675bad2e0c8f9895f12002cd29cf15890 into fafa75d5cb9e4589e71badf11af680bc0dff6941

--- sdk/subscription/Azure.ResourceManager.Subscription/samples/Generated/Samples/Sample_TenantResourceExtensions.cs
+++ sdk/subscription/Azure.ResourceManager.Subscription/samples/Generated/Samples/Sample_TenantResourceExtensions.cs
@@ -75,5 +75,29 @@ namespace Azure.ResourceManager.Subscription.Samples
 
             Console.WriteLine($"Succeeded: {result}");
         }
+
+        // getPendingSubscriptionOperations
+        [NUnit.Framework.Test]
+        [NUnit.Framework.Ignore("Only verifying that the sample builds")]
+        public async Task GetSubscriptionOperation_GetPendingSubscriptionOperations()
+        {
+            // Generated from example definition: specification/subscription/resource-manager/Microsoft.Subscription/stable/2021-10-01/examples/getSubscriptionOperation.json
+            // this example is just showing the usage of "SubscriptionOperation_Get" operation, for the dependent resources, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this TenantResource created on azure
+            // for more information of creating TenantResource, please refer to the document of TenantResource
+            var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+
+            // invoke the operation
+            string operationId = "e4b8d068-f574-462a-a76f-6fa0afc613c9";
+            SubscriptionCreationResult result = await tenantResource.GetSubscriptionOperationAsync(operationId);
+
+            Console.WriteLine($"Succeeded: {result}");
+        }
     }
 }
