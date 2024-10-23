14f46da54aa07d68e68cde2ef7cffb790955244f
SDKAuto <sdkautomation@microsoft.com>
2024-10-23 21:08:33 +0000

    CodeGen from PR 31071 in Azure/azure-rest-api-specs Merge c3a3162655847a7ef92fdfd35859e2ac6a912461 into 0f300277e21972f20b32ffbff96180217875909b

--- sdk/mongocluster/Azure.ResourceManager.MongoCluster/samples/Generated/Samples/Sample_MongoClusterPrivateEndpointConnectionResourceCollection.cs
+++ sdk/mongocluster/Azure.ResourceManager.MongoCluster/samples/Generated/Samples/Sample_MongoClusterPrivateEndpointConnectionResourceCollection.cs
@@ -20,7 +20,7 @@ namespace Azure.ResourceManager.MongoCluster.Samples
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
         public async Task GetAll_ListsThePrivateEndpointConnectionResourcesOnAMongoClusterResource()
         {
-            // Generated from example definition: 2024-07-01/MongoClusters_PrivateEndpointConnectionList.json
+            // Generated from example definition: 2024-10-01-preview/MongoClusters_PrivateEndpointConnectionList.json
             // this example is just showing the usage of "PrivateEndpointConnectionResource_ListByMongoCluster" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -57,7 +57,7 @@ namespace Azure.ResourceManager.MongoCluster.Samples
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
         public async Task Get_GetAPrivateEndpointConnectionOnAMongoClusterResource()
         {
-            // Generated from example definition: 2024-07-01/MongoClusters_PrivateEndpointConnectionGet.json
+            // Generated from example definition: 2024-10-01-preview/MongoClusters_PrivateEndpointConnectionGet.json
             // this example is just showing the usage of "PrivateEndpointConnectionResource_Get" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -92,7 +92,7 @@ namespace Azure.ResourceManager.MongoCluster.Samples
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
         public async Task Exists_GetAPrivateEndpointConnectionOnAMongoClusterResource()
         {
-            // Generated from example definition: 2024-07-01/MongoClusters_PrivateEndpointConnectionGet.json
+            // Generated from example definition: 2024-10-01-preview/MongoClusters_PrivateEndpointConnectionGet.json
             // this example is just showing the usage of "PrivateEndpointConnectionResource_Get" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -123,7 +123,7 @@ namespace Azure.ResourceManager.MongoCluster.Samples
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
         public async Task GetIfExists_GetAPrivateEndpointConnectionOnAMongoClusterResource()
         {
-            // Generated from example definition: 2024-07-01/MongoClusters_PrivateEndpointConnectionGet.json
+            // Generated from example definition: 2024-10-01-preview/MongoClusters_PrivateEndpointConnectionGet.json
             // this example is just showing the usage of "PrivateEndpointConnectionResource_Get" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -166,7 +166,7 @@ namespace Azure.ResourceManager.MongoCluster.Samples
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
         public async Task CreateOrUpdate_ApprovesAPrivateEndpointConnectionOnAMongoClusterResource()
         {
-            // Generated from example definition: 2024-07-01/MongoClusters_PrivateEndpointConnectionPut.json
+            // Generated from example definition: 2024-10-01-preview/MongoClusters_PrivateEndpointConnectionPut.json
             // this example is just showing the usage of "PrivateEndpointConnectionResource_Create" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
