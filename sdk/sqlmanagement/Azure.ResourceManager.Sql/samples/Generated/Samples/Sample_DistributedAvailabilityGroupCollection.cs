bc49ab9ef56fe270b70c88933ea76bc4a40e4af2
SDKAuto <sdkautomation@microsoft.com>
2024-10-30 11:30:53 +0000

    CodeGen from PR 31332 in Azure/azure-rest-api-specs Merge cd4aeb7d2182ca8e3781e39740ac5d30dc91d7b4 into 8abf88fb9caa0bfea8d4026a4b7628bbe47961ed

--- sdk/sqlmanagement/Azure.ResourceManager.Sql/samples/Generated/Samples/Sample_DistributedAvailabilityGroupCollection.cs
+++ sdk/sqlmanagement/Azure.ResourceManager.Sql/samples/Generated/Samples/Sample_DistributedAvailabilityGroupCollection.cs
@@ -9,6 +9,7 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
+using Azure.ResourceManager.Sql.Models;
 
 namespace Azure.ResourceManager.Sql.Samples
 {
@@ -19,7 +20,7 @@ namespace Azure.ResourceManager.Sql.Samples
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
         public async Task GetAll_ListsAllDistributedAvailabilityGroupsInInstance()
         {
-            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2021-11-01-preview/examples/DistributedAvailabilityGroupsListByInstance.json
+            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2023-08-01-preview/examples/DistributedAvailabilityGroupsListByInstance.json
             // this example is just showing the usage of "DistributedAvailabilityGroups_ListByInstance" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -56,7 +57,7 @@ namespace Azure.ResourceManager.Sql.Samples
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
         public async Task Get_GetsTheDistributedAvailabilityGroupInfo()
         {
-            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2021-11-01-preview/examples/DistributedAvailabilityGroupsGet.json
+            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2023-08-01-preview/examples/DistributedAvailabilityGroupsGet.json
             // this example is just showing the usage of "DistributedAvailabilityGroups_Get" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -66,7 +67,7 @@ namespace Azure.ResourceManager.Sql.Samples
 
             // this example assumes you already have this ManagedInstanceResource created on azure
             // for more information of creating ManagedInstanceResource, please refer to the document of ManagedInstanceResource
-            string subscriptionId = "f2669dff-5f08-45dd-b857-b2a60b72cdc9";
+            string subscriptionId = "00000000-1111-2222-3333-444444444444";
             string resourceGroupName = "testrg";
             string managedInstanceName = "testcl";
             ResourceIdentifier managedInstanceResourceId = ManagedInstanceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, managedInstanceName);
@@ -91,7 +92,7 @@ namespace Azure.ResourceManager.Sql.Samples
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
         public async Task Exists_GetsTheDistributedAvailabilityGroupInfo()
         {
-            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2021-11-01-preview/examples/DistributedAvailabilityGroupsGet.json
+            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2023-08-01-preview/examples/DistributedAvailabilityGroupsGet.json
             // this example is just showing the usage of "DistributedAvailabilityGroups_Get" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -101,7 +102,7 @@ namespace Azure.ResourceManager.Sql.Samples
 
             // this example assumes you already have this ManagedInstanceResource created on azure
             // for more information of creating ManagedInstanceResource, please refer to the document of ManagedInstanceResource
-            string subscriptionId = "f2669dff-5f08-45dd-b857-b2a60b72cdc9";
+            string subscriptionId = "00000000-1111-2222-3333-444444444444";
             string resourceGroupName = "testrg";
             string managedInstanceName = "testcl";
             ResourceIdentifier managedInstanceResourceId = ManagedInstanceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, managedInstanceName);
@@ -122,7 +123,7 @@ namespace Azure.ResourceManager.Sql.Samples
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
         public async Task GetIfExists_GetsTheDistributedAvailabilityGroupInfo()
         {
-            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2021-11-01-preview/examples/DistributedAvailabilityGroupsGet.json
+            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2023-08-01-preview/examples/DistributedAvailabilityGroupsGet.json
             // this example is just showing the usage of "DistributedAvailabilityGroups_Get" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -132,7 +133,7 @@ namespace Azure.ResourceManager.Sql.Samples
 
             // this example assumes you already have this ManagedInstanceResource created on azure
             // for more information of creating ManagedInstanceResource, please refer to the document of ManagedInstanceResource
-            string subscriptionId = "f2669dff-5f08-45dd-b857-b2a60b72cdc9";
+            string subscriptionId = "00000000-1111-2222-3333-444444444444";
             string resourceGroupName = "testrg";
             string managedInstanceName = "testcl";
             ResourceIdentifier managedInstanceResourceId = ManagedInstanceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, managedInstanceName);
@@ -160,12 +161,12 @@ namespace Azure.ResourceManager.Sql.Samples
             }
         }
 
-        // Create a distributed availability group.
+        // Create a distributed availability group with all properties.
         [NUnit.Framework.Test]
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
-        public async Task CreateOrUpdate_CreateADistributedAvailabilityGroup()
+        public async Task CreateOrUpdate_CreateADistributedAvailabilityGroupWithAllProperties()
         {
-            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2021-11-01-preview/examples/DistributedAvailabilityGroupsCreate.json
+            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2023-08-01-preview/examples/DistributedAvailabilityGroupsCreateMax.json
             // this example is just showing the usage of "DistributedAvailabilityGroups_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -188,10 +189,68 @@ namespace Azure.ResourceManager.Sql.Samples
             string distributedAvailabilityGroupName = "dag";
             DistributedAvailabilityGroupData data = new DistributedAvailabilityGroupData()
             {
-                TargetDatabase = "testdb",
-                SourceEndpoint = "TCP://SERVER:7022",
-                PrimaryAvailabilityGroupName = "BoxLocalAg1",
-                SecondaryAvailabilityGroupName = "testcl",
+                PartnerAvailabilityGroupName = "BoxLocalAg1",
+                PartnerEndpoint = "TCP://SERVER:7022",
+                InstanceLinkRole = LinkRole.Primary,
+                InstanceAvailabilityGroupName = "testcl",
+                FailoverMode = FailoverModeType.None,
+                SeedingMode = SeedingModeType.Automatic,
+                Databases =
+{
+new DistributedAvailabilityGroupDatabase()
+{
+DatabaseName = "testdb",
+}
+},
+            };
+            ArmOperation<DistributedAvailabilityGroupResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, distributedAvailabilityGroupName, data);
+            DistributedAvailabilityGroupResource result = lro.Value;
+
+            // the variable result is a resource, you could call other operations on this instance as well
+            // but just for demo, we get its data from this resource instance
+            DistributedAvailabilityGroupData resourceData = result.Data;
+            // for demo we just print out the id
+            Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+        }
+
+        // Create a distributed availability group with minimal properties.
+        [NUnit.Framework.Test]
+        [NUnit.Framework.Ignore("Only verifying that the sample builds")]
+        public async Task CreateOrUpdate_CreateADistributedAvailabilityGroupWithMinimalProperties()
+        {
+            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2023-08-01-preview/examples/DistributedAvailabilityGroupsCreateMin.json
+            // this example is just showing the usage of "DistributedAvailabilityGroups_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this ManagedInstanceResource created on azure
+            // for more information of creating ManagedInstanceResource, please refer to the document of ManagedInstanceResource
+            string subscriptionId = "00000000-1111-2222-3333-444444444444";
+            string resourceGroupName = "testrg";
+            string managedInstanceName = "testcl";
+            ResourceIdentifier managedInstanceResourceId = ManagedInstanceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, managedInstanceName);
+            ManagedInstanceResource managedInstance = client.GetManagedInstanceResource(managedInstanceResourceId);
+
+            // get the collection of this DistributedAvailabilityGroupResource
+            DistributedAvailabilityGroupCollection collection = managedInstance.GetDistributedAvailabilityGroups();
+
+            // invoke the operation
+            string distributedAvailabilityGroupName = "dag";
+            DistributedAvailabilityGroupData data = new DistributedAvailabilityGroupData()
+            {
+                PartnerAvailabilityGroupName = "BoxLocalAg1",
+                PartnerEndpoint = "TCP://SERVER:7022",
+                InstanceAvailabilityGroupName = "testcl",
+                Databases =
+{
+new DistributedAvailabilityGroupDatabase()
+{
+DatabaseName = "testdb",
+}
+},
             };
             ArmOperation<DistributedAvailabilityGroupResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, distributedAvailabilityGroupName, data);
             DistributedAvailabilityGroupResource result = lro.Value;
