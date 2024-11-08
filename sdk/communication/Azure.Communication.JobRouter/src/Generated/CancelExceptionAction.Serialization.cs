f6757b2a0a62ed7bfd46fce0573fc11860e19782
SDKAuto <sdkautomation@microsoft.com>
2024-11-08 03:22:18 +0000

    CodeGen from PR 3559 in test-repo-billy/azure-rest-api-specs Merge 060e30d7a05928d0442267354fe4ccf0740e37ef into a1ece0d8092cbabd104c6a090519eb102db0c350

--- sdk/communication/Azure.Communication.JobRouter/src/Generated/CancelExceptionAction.Serialization.cs
+++ sdk/communication/Azure.Communication.JobRouter/src/Generated/CancelExceptionAction.Serialization.cs
@@ -19,22 +19,13 @@ namespace Azure.Communication.JobRouter
 
         void IJsonModel<CancelExceptionAction>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
-            writer.WriteStartObject();
-            JsonModelWriteCore(writer, options);
-            writer.WriteEndObject();
-        }
-
-        /// <param name="writer"> The JSON writer. </param>
-        /// <param name="options"> The client options for reading and writing models. </param>
-        protected override void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
-        {
             var format = options.Format == "W" ? ((IPersistableModel<CancelExceptionAction>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(CancelExceptionAction)} does not support writing '{format}' format.");
             }
 
-            base.JsonModelWriteCore(writer, options);
+            writer.WriteStartObject();
             if (Optional.IsDefined(Note))
             {
                 writer.WritePropertyName("note"u8);
@@ -45,6 +36,29 @@ namespace Azure.Communication.JobRouter
                 writer.WritePropertyName("dispositionCode"u8);
                 writer.WriteStringValue(DispositionCode);
             }
+            if (Optional.IsDefined(Id))
+            {
+                writer.WritePropertyName("id"u8);
+                writer.WriteStringValue(Id);
+            }
+            writer.WritePropertyName("kind"u8);
+            writer.WriteStringValue(Kind.ToString());
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
+            writer.WriteEndObject();
         }
 
         CancelExceptionAction IJsonModel<CancelExceptionAction>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
