7689dc8d90b1e1f8e6fc2d7bc35ae072a87b7a5d
SDKAuto <sdkautomation@microsoft.com>
2024-12-04 19:31:55 +0000

    CodeGen from PR 31742 in Azure/azure-rest-api-specs Merge 896204f0f32fbaa1080389d8f799d29bfc6f62a6 into c2d07b8aa83bbd091ec6391b8559f2f2a56ef846

--- sdk/eventgrid/Azure.Messaging.EventGrid.SystemEvents/src/Generated/AcsMessageMediaContent.Serialization.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid.SystemEvents/src/Generated/AcsMessageMediaContent.Serialization.cs
@@ -54,6 +54,11 @@ namespace Azure.Messaging.EventGrid.SystemEvents
                 writer.WritePropertyName("caption"u8);
                 writer.WriteStringValue(Caption);
             }
+            if (Optional.IsDefined(Animated))
+            {
+                writer.WritePropertyName("animated"u8);
+                writer.WriteBooleanValue(Animated.Value);
+            }
             if (options.Format != "W" && _serializedAdditionalRawData != null)
             {
                 foreach (var item in _serializedAdditionalRawData)
@@ -95,6 +100,7 @@ namespace Azure.Messaging.EventGrid.SystemEvents
             string id = default;
             string fileName = default;
             string caption = default;
+            bool? animated = default;
             IDictionary<string, BinaryData> serializedAdditionalRawData = default;
             Dictionary<string, BinaryData> rawDataDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
@@ -119,13 +125,28 @@ namespace Azure.Messaging.EventGrid.SystemEvents
                     caption = property.Value.GetString();
                     continue;
                 }
+                if (property.NameEquals("animated"u8))
+                {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        continue;
+                    }
+                    animated = property.Value.GetBoolean();
+                    continue;
+                }
                 if (options.Format != "W")
                 {
                     rawDataDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
                 }
             }
             serializedAdditionalRawData = rawDataDictionary;
-            return new AcsMessageMediaContent(mimeType, id, fileName, caption, serializedAdditionalRawData);
+            return new AcsMessageMediaContent(
+                mimeType,
+                id,
+                fileName,
+                caption,
+                animated,
+                serializedAdditionalRawData);
         }
 
         BinaryData IPersistableModel<AcsMessageMediaContent>.Write(ModelReaderWriterOptions options)
