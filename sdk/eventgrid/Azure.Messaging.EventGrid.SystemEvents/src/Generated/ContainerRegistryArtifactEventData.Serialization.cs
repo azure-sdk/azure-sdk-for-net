6617f4032a691e431136cfe59f82377da6406d33
SDKAuto <sdkautomation@microsoft.com>
2024-12-05 20:24:55 +0000

    CodeGen from PR 31775 in Azure/azure-rest-api-specs Merge fa50f39a255b1b61526d07a071d56e5ad0445fe9 into cd5fa3a6b8fcec66018ba785582888974b76f30f

--- sdk/eventgrid/Azure.Messaging.EventGrid.SystemEvents/src/Generated/ContainerRegistryArtifactEventData.Serialization.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid.SystemEvents/src/Generated/ContainerRegistryArtifactEventData.Serialization.cs
@@ -39,8 +39,11 @@ namespace Azure.Messaging.EventGrid.SystemEvents
                 writer.WritePropertyName("id"u8);
                 writer.WriteStringValue(Id);
             }
-            writer.WritePropertyName("timestamp"u8);
-            writer.WriteStringValue(Timestamp, "O");
+            if (Optional.IsDefined(Timestamp))
+            {
+                writer.WritePropertyName("timestamp"u8);
+                writer.WriteStringValue(Timestamp.Value, "O");
+            }
             if (Optional.IsDefined(Action))
             {
                 writer.WritePropertyName("action"u8);
@@ -93,7 +96,7 @@ namespace Azure.Messaging.EventGrid.SystemEvents
                 return null;
             }
             string id = default;
-            DateTimeOffset timestamp = default;
+            DateTimeOffset? timestamp = default;
             string action = default;
             string location = default;
             ContainerRegistryArtifactEventTarget target = default;
@@ -109,6 +112,10 @@ namespace Azure.Messaging.EventGrid.SystemEvents
                 }
                 if (property.NameEquals("timestamp"u8))
                 {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        continue;
+                    }
                     timestamp = property.Value.GetDateTimeOffset("O");
                     continue;
                 }
