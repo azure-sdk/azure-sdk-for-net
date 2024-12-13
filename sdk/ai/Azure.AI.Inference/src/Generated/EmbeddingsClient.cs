a70d7d6f39c9f4bf050495817d0c1bc1dcfd5e7f
SDKAuto <sdkautomation@microsoft.com>
2024-12-13 00:26:30 +0000

    CodeGen from PR 31869 in Azure/azure-rest-api-specs Merge 19827a6172c178329560616fdbec7869de02b0b0 into e71bdd99973262ffdefaf9ed2f73e92d5d1f7091

--- sdk/ai/Azure.AI.Inference/src/Generated/EmbeddingsClient.cs
+++ sdk/ai/Azure.AI.Inference/src/Generated/EmbeddingsClient.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
@@ -70,6 +71,92 @@ namespace Azure.AI.Inference
             _pipeline = HttpPipelineBuilder.Build(options, Array.Empty<HttpPipelinePolicy>(), new HttpPipelinePolicy[] { new BearerTokenAuthenticationPolicy(_tokenCredential, AuthorizationScopes) }, new ResponseClassifier());
             _endpoint = endpoint;
             _apiVersion = options.Version;
+        }
+
+        /// <summary>
+        /// Return the embedding vectors for given text prompts.
+        /// The method makes a REST API call to the `/embeddings` route on the given endpoint.
+        /// </summary>
+        /// <param name="input">
+        /// Input text to embed, encoded as a string or array of tokens.
+        /// To embed multiple inputs in a single request, pass an array
+        /// of strings or array of token arrays.
+        /// </param>
+        /// <param name="dimensions">
+        /// Optional. The number of dimensions the resulting output embeddings should have.
+        /// Passing null causes the model to use its default value.
+        /// Returns a 422 error if the model doesn't support the value or parameter.
+        /// </param>
+        /// <param name="encodingFormat"> Optional. The desired format for the returned embeddings. </param>
+        /// <param name="inputType">
+        /// Optional. The type of the input.
+        /// Returns a 422 error if the model doesn't support the value or parameter.
+        /// </param>
+        /// <param name="model"> ID of the specific AI model to use, if more than one model is available on the endpoint. </param>
+        /// <param name="extraParams">
+        /// Controls what happens if extra parameters, undefined by the REST API,
+        /// are passed in the JSON request payload.
+        /// This sets the HTTP request header `extra-parameters`.
+        /// </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="input"/> is null. </exception>
+        internal virtual async Task<Response<EmbeddingsResult>> EmbedAsync(IEnumerable<string> input, int? dimensions = null, EmbeddingEncodingFormat? encodingFormat = null, EmbeddingInputType? inputType = null, string model = null, ExtraParameters? extraParams = null, CancellationToken cancellationToken = default)
+        {
+            Argument.AssertNotNull(input, nameof(input));
+
+            EmbedRequest embedRequest = new EmbedRequest(
+                input.ToList(),
+                dimensions,
+                encodingFormat,
+                inputType,
+                model,
+                null);
+            RequestContext context = FromCancellationToken(cancellationToken);
+            Response response = await EmbedAsync(embedRequest.ToRequestContent(), extraParams?.ToString(), context).ConfigureAwait(false);
+            return Response.FromValue(EmbeddingsResult.FromResponse(response), response);
+        }
+
+        /// <summary>
+        /// Return the embedding vectors for given text prompts.
+        /// The method makes a REST API call to the `/embeddings` route on the given endpoint.
+        /// </summary>
+        /// <param name="input">
+        /// Input text to embed, encoded as a string or array of tokens.
+        /// To embed multiple inputs in a single request, pass an array
+        /// of strings or array of token arrays.
+        /// </param>
+        /// <param name="dimensions">
+        /// Optional. The number of dimensions the resulting output embeddings should have.
+        /// Passing null causes the model to use its default value.
+        /// Returns a 422 error if the model doesn't support the value or parameter.
+        /// </param>
+        /// <param name="encodingFormat"> Optional. The desired format for the returned embeddings. </param>
+        /// <param name="inputType">
+        /// Optional. The type of the input.
+        /// Returns a 422 error if the model doesn't support the value or parameter.
+        /// </param>
+        /// <param name="model"> ID of the specific AI model to use, if more than one model is available on the endpoint. </param>
+        /// <param name="extraParams">
+        /// Controls what happens if extra parameters, undefined by the REST API,
+        /// are passed in the JSON request payload.
+        /// This sets the HTTP request header `extra-parameters`.
+        /// </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="input"/> is null. </exception>
+        internal virtual Response<EmbeddingsResult> Embed(IEnumerable<string> input, int? dimensions = null, EmbeddingEncodingFormat? encodingFormat = null, EmbeddingInputType? inputType = null, string model = null, ExtraParameters? extraParams = null, CancellationToken cancellationToken = default)
+        {
+            Argument.AssertNotNull(input, nameof(input));
+
+            EmbedRequest embedRequest = new EmbedRequest(
+                input.ToList(),
+                dimensions,
+                encodingFormat,
+                inputType,
+                model,
+                null);
+            RequestContext context = FromCancellationToken(cancellationToken);
+            Response response = Embed(embedRequest.ToRequestContent(), extraParams?.ToString(), context);
+            return Response.FromValue(EmbeddingsResult.FromResponse(response), response);
         }
 
         /// <summary>
