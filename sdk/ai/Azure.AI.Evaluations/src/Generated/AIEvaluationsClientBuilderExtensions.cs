c6a47fdf9737ab1216cc771edad31db9e7166508
SDKAuto <sdkautomation@microsoft.com>
2025-02-10 18:17:43 +0000

    CodeGen from PR 32524 in Azure/azure-rest-api-specs Merge e8a40199e62adbb2d233742d6205b76f5f2db854 into 85b13254060355ccbad6317d33f647487dd57ced

--- sdk/ai/Azure.AI.Evaluations/src/Generated/AIEvaluationsClientBuilderExtensions.cs
+++ sdk/ai/Azure.AI.Evaluations/src/Generated/AIEvaluationsClientBuilderExtensions.cs
@@ -6,33 +6,33 @@
 #nullable disable
 
 using System;
-using Azure.AI.Projects;
+using Azure.AI.Evaluations;
 using Azure.Core.Extensions;
 
 namespace Microsoft.Extensions.Azure
 {
-    /// <summary> Extension methods to add <see cref="AgentsClient"/> to client builder. </summary>
-    public static partial class AIProjectsClientBuilderExtensions
+    /// <summary> Extension methods to add <see cref="AIEvaluationsClient"/> to client builder. </summary>
+    public static partial class AIEvaluationsClientBuilderExtensions
     {
-        /// <summary> Registers a <see cref="AgentsClient"/> instance. </summary>
+        /// <summary> Registers a <see cref="AIEvaluationsClient"/> instance. </summary>
         /// <param name="builder"> The builder to register with. </param>
         /// <param name="endpoint"> The Azure AI Foundry project endpoint, in the form `https://&lt;azure-region&gt;.api.azureml.ms` or `https://&lt;private-link-guid&gt;.&lt;azure-region&gt;.api.azureml.ms`, where &lt;azure-region&gt; is the Azure region where the project is deployed (e.g. westus) and &lt;private-link-guid&gt; is the GUID of the Enterprise private link. </param>
         /// <param name="subscriptionId"> The Azure subscription ID. </param>
         /// <param name="resourceGroupName"> The name of the Azure Resource Group. </param>
         /// <param name="projectName"> The Azure AI Foundry project name. </param>
-        public static IAzureClientBuilder<AgentsClient, AgentsClientOptions> AddAgentsClient<TBuilder>(this TBuilder builder, Uri endpoint, string subscriptionId, string resourceGroupName, string projectName)
+        public static IAzureClientBuilder<AIEvaluationsClient, AIEvaluationsClientOptions> AddAIEvaluationsClient<TBuilder>(this TBuilder builder, Uri endpoint, string subscriptionId, string resourceGroupName, string projectName)
         where TBuilder : IAzureClientFactoryBuilderWithCredential
         {
-            return builder.RegisterClientFactory<AgentsClient, AgentsClientOptions>((options, cred) => new AgentsClient(endpoint, subscriptionId, resourceGroupName, projectName, cred, options));
+            return builder.RegisterClientFactory<AIEvaluationsClient, AIEvaluationsClientOptions>((options, cred) => new AIEvaluationsClient(endpoint, subscriptionId, resourceGroupName, projectName, cred, options));
         }
 
-        /// <summary> Registers a <see cref="AgentsClient"/> instance. </summary>
+        /// <summary> Registers a <see cref="AIEvaluationsClient"/> instance. </summary>
         /// <param name="builder"> The builder to register with. </param>
         /// <param name="configuration"> The configuration values. </param>
-        public static IAzureClientBuilder<AgentsClient, AgentsClientOptions> AddAgentsClient<TBuilder, TConfiguration>(this TBuilder builder, TConfiguration configuration)
+        public static IAzureClientBuilder<AIEvaluationsClient, AIEvaluationsClientOptions> AddAIEvaluationsClient<TBuilder, TConfiguration>(this TBuilder builder, TConfiguration configuration)
         where TBuilder : IAzureClientFactoryBuilderWithConfiguration<TConfiguration>
         {
-            return builder.RegisterClientFactory<AgentsClient, AgentsClientOptions>(configuration);
+            return builder.RegisterClientFactory<AIEvaluationsClient, AIEvaluationsClientOptions>(configuration);
         }
     }
 }
