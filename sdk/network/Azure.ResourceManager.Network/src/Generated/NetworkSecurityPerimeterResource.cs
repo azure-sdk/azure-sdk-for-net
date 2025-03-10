bd96334b7d8c21c01c675abbdef4926d65cc2597
SDKAuto <sdkautomation@microsoft.com>
2025-03-10 14:35:01 +0000

    CodeGen from PR 33099 in Azure/azure-rest-api-specs Merge 1b2552a09a4ac6ba89eeefd4bf59b95d1af259be into 2783c701152e06c6c02bbd10b012670324544aad

--- sdk/network/Azure.ResourceManager.Network/src/Generated/NetworkSecurityPerimeterResource.cs
+++ sdk/network/Azure.ResourceManager.Network/src/Generated/NetworkSecurityPerimeterResource.cs
@@ -537,15 +537,16 @@ namespace Azure.ResourceManager.Network
         /// </list>
         /// </summary>
         /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
+        /// <param name="forceDeletion"> Deletes the resource even if it contains any child associations. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
-        public virtual async Task<ArmOperation> DeleteAsync(WaitUntil waitUntil, CancellationToken cancellationToken = default)
+        public virtual async Task<ArmOperation> DeleteAsync(WaitUntil waitUntil, bool? forceDeletion = null, CancellationToken cancellationToken = default)
         {
             using var scope = _networkSecurityPerimeterClientDiagnostics.CreateScope("NetworkSecurityPerimeterResource.Delete");
             scope.Start();
             try
             {
-                var response = await _networkSecurityPerimeterRestClient.DeleteAsync(Id.SubscriptionId, Id.ResourceGroupName, Id.Name, cancellationToken).ConfigureAwait(false);
-                var operation = new NetworkArmOperation(_networkSecurityPerimeterClientDiagnostics, Pipeline, _networkSecurityPerimeterRestClient.CreateDeleteRequest(Id.SubscriptionId, Id.ResourceGroupName, Id.Name).Request, response, OperationFinalStateVia.Location);
+                var response = await _networkSecurityPerimeterRestClient.DeleteAsync(Id.SubscriptionId, Id.ResourceGroupName, Id.Name, forceDeletion, cancellationToken).ConfigureAwait(false);
+                var operation = new NetworkArmOperation(_networkSecurityPerimeterClientDiagnostics, Pipeline, _networkSecurityPerimeterRestClient.CreateDeleteRequest(Id.SubscriptionId, Id.ResourceGroupName, Id.Name, forceDeletion).Request, response, OperationFinalStateVia.Location);
                 if (waitUntil == WaitUntil.Completed)
                     await operation.WaitForCompletionResponseAsync(cancellationToken).ConfigureAwait(false);
                 return operation;
@@ -579,15 +580,16 @@ namespace Azure.ResourceManager.Network
         /// </list>
         /// </summary>
         /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
+        /// <param name="forceDeletion"> Deletes the resource even if it contains any child associations. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
-        public virtual ArmOperation Delete(WaitUntil waitUntil, CancellationToken cancellationToken = default)
+        public virtual ArmOperation Delete(WaitUntil waitUntil, bool? forceDeletion = null, CancellationToken cancellationToken = default)
         {
             using var scope = _networkSecurityPerimeterClientDiagnostics.CreateScope("NetworkSecurityPerimeterResource.Delete");
             scope.Start();
             try
             {
-                var response = _networkSecurityPerimeterRestClient.Delete(Id.SubscriptionId, Id.ResourceGroupName, Id.Name, cancellationToken);
-                var operation = new NetworkArmOperation(_networkSecurityPerimeterClientDiagnostics, Pipeline, _networkSecurityPerimeterRestClient.CreateDeleteRequest(Id.SubscriptionId, Id.ResourceGroupName, Id.Name).Request, response, OperationFinalStateVia.Location);
+                var response = _networkSecurityPerimeterRestClient.Delete(Id.SubscriptionId, Id.ResourceGroupName, Id.Name, forceDeletion, cancellationToken);
+                var operation = new NetworkArmOperation(_networkSecurityPerimeterClientDiagnostics, Pipeline, _networkSecurityPerimeterRestClient.CreateDeleteRequest(Id.SubscriptionId, Id.ResourceGroupName, Id.Name, forceDeletion).Request, response, OperationFinalStateVia.Location);
                 if (waitUntil == WaitUntil.Completed)
                     operation.WaitForCompletionResponse(cancellationToken);
                 return operation;
