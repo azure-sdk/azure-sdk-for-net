cd36b3dbfd06cd919b6aaa7edada75717341e931
SDKAuto <sdkautomation@microsoft.com>
2025-03-24 19:27:14 +0000

    CodeGen from PR 33457 in Azure/azure-rest-api-specs Merge 6ce02b281d79d2e8fe26dbb4991d56e8f370e9fa into 13a832388721b04e485734af0ea97e25a4556e14

--- sdk/ai/Azure.AI.Projects/src/Generated/RunStepToolCall.Serialization.cs
+++ sdk/ai/Azure.AI.Projects/src/Generated/RunStepToolCall.Serialization.cs
@@ -80,6 +80,7 @@ namespace Azure.AI.Projects
                 switch (discriminator.GetString())
                 {
                     case "azure_ai_search": return RunStepAzureAISearchToolCall.DeserializeRunStepAzureAISearchToolCall(element, options);
+                    case "bing_custom_search": return RunStepCustomSearchToolCall.DeserializeRunStepCustomSearchToolCall(element, options);
                     case "bing_grounding": return RunStepBingGroundingToolCall.DeserializeRunStepBingGroundingToolCall(element, options);
                     case "code_interpreter": return RunStepCodeInterpreterToolCall.DeserializeRunStepCodeInterpreterToolCall(element, options);
                     case "fabric_dataagent": return RunStepMicrosoftFabricToolCall.DeserializeRunStepMicrosoftFabricToolCall(element, options);
