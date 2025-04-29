64a90de869827763111eca142c44aecf6814acea
SDKAuto <sdkautomation@microsoft.com>
2025-04-29 21:23:43 +0000

    CodeGen from PR 34347 in Azure/azure-rest-api-specs Merge ef5a324ffeee700fd0d1e4008b141db5efeb4466 into 2b0c1e3f5205502176451676422b6be137b1e5e5

--- sdk/standbypool/Azure.ResourceManager.StandbyPool/src/Generated/Models/StandbyContainerGroupPoolRuntimeViewProperties.Serialization.cs
+++ sdk/standbypool/Azure.ResourceManager.StandbyPool/src/Generated/Models/StandbyContainerGroupPoolRuntimeViewProperties.Serialization.cs
@@ -97,7 +97,7 @@ namespace Azure.ResourceManager.StandbyPool.Models
                 return null;
             }
             IReadOnlyList<ContainerGroupInstanceCountSummary> instanceCountSummary = default;
-            StandbyPoolStatus status = default;
+            PoolStatus status = default;
             StandbyProvisioningState? provisioningState = default;
             StandbyContainerGroupPoolPrediction prediction = default;
             IDictionary<string, BinaryData> serializedAdditionalRawData = default;
@@ -120,7 +120,7 @@ namespace Azure.ResourceManager.StandbyPool.Models
                     {
                         continue;
                     }
-                    status = StandbyPoolStatus.DeserializeStandbyPoolStatus(property.Value, options);
+                    status = PoolStatus.DeserializePoolStatus(property.Value, options);
                     continue;
                 }
                 if (property.NameEquals("provisioningState"u8))
