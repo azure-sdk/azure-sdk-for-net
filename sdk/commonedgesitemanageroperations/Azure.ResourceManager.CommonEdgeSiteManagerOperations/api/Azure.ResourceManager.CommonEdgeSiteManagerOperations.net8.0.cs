fa46a32c488607d1156cfa55623c0a216223e8be
SDKAuto <sdkautomation@microsoft.com>
2025-05-09 02:46:22 +0000

    CodeGen from PR 34492 in Azure/azure-rest-api-specs Merge 996fce95c4a7d354249d25636b36adfc09f2804c into 9f69db7532b6706baedc004f2eda762196a3e1eb

---
+++ sdk/commonedgesitemanageroperations/Azure.ResourceManager.CommonEdgeSiteManagerOperations/api/Azure.ResourceManager.CommonEdgeSiteManagerOperations.net8.0.cs
@@ -0,0 +1,9 @@
+namespace Azure.ResourceManager.CommonEdgeSiteManagerOperations
+{
+    public partial class AzureResourceManagerCommonEdgeSiteManagerOperationsContext : System.ClientModel.Primitives.ModelReaderWriterContext
+    {
+        internal AzureResourceManagerCommonEdgeSiteManagerOperationsContext() { }
+        public static Azure.ResourceManager.CommonEdgeSiteManagerOperations.AzureResourceManagerCommonEdgeSiteManagerOperationsContext Default { get { throw null; } }
+        protected override bool TryGetTypeBuilderCore(System.Type type, out System.ClientModel.Primitives.ModelReaderWriterTypeBuilder builder) { throw null; }
+    }
+}
