564047c9ce71bdcd84712484b1f1ed876419d4ba
azure-sdk <azuresdk@microsoft.com>
2025-05-26 09:16:34 +0000

    Update SDK codes da_he_2

--- sdk/hardwaresecuritymodules/Azure.ResourceManager.HardwareSecurityModules/tests/Generated/Samples/Sample_DedicatedHsmResource.cs
+++ sdk/hardwaresecuritymodules/Azure.ResourceManager.HardwareSecurityModules/tests/Generated/Samples/Sample_DedicatedHsmResource.cs
@@ -78,36 +78,6 @@ namespace Azure.ResourceManager.HardwareSecurityModules.Samples
 
         [Test]
         [Ignore("Only validating compilation of examples")]
-        public async Task Get_GetAPaymentHSMWith20181031PreviewApiVersion()
-        {
-            // Generated from example definition: specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/preview/2024-06-30-preview/examples/PaymentHsm_Get_With_2018-10-31Preview_Version.json
-            // this example is just showing the usage of "DedicatedHsm_Get" operation, for the dependent resources, they will have to be created separately.
-
-            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
-            TokenCredential cred = new DefaultAzureCredential();
-            // authenticate your client
-            ArmClient client = new ArmClient(cred);
-
-            // this example assumes you already have this DedicatedHsmResource created on azure
-            // for more information of creating DedicatedHsmResource, please refer to the document of DedicatedHsmResource
-            string subscriptionId = "00000000-0000-0000-0000-000000000000";
-            string resourceGroupName = "hsm-group";
-            string name = "hsm1";
-            ResourceIdentifier dedicatedHsmResourceId = DedicatedHsmResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, name);
-            DedicatedHsmResource dedicatedHsm = client.GetDedicatedHsmResource(dedicatedHsmResourceId);
-
-            // invoke the operation
-            DedicatedHsmResource result = await dedicatedHsm.GetAsync();
-
-            // the variable result is a resource, you could call other operations on this instance as well
-            // but just for demo, we get its data from this resource instance
-            DedicatedHsmData resourceData = result.Data;
-            // for demo we just print out the id
-            Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-        }
-
-        [Test]
-        [Ignore("Only validating compilation of examples")]
         public async Task Delete_DeleteADedicatedHSM()
         {
             // Generated from example definition: specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/preview/2024-06-30-preview/examples/DedicatedHsm_Delete.json
