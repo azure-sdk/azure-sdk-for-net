b4d8a58e5018004ab2cac3712e354d73c73cb1bc
azure-sdk <azuresdk@microsoft.com>
2025-05-26 09:12:06 +0000

    Update SDK codes sq_wo_6

--- sdk/storage/Azure.ResourceManager.Storage/samples/Generated/Samples/Sample_StorageQueueCollection.cs
+++ sdk/storage/Azure.ResourceManager.Storage/samples/Generated/Samples/Sample_StorageQueueCollection.cs
@@ -17,42 +17,6 @@ namespace Azure.ResourceManager.Storage.Samples
     {
         [Test]
         [Ignore("Only validating compilation of examples")]
-        public async Task CreateOrUpdate_QueueOperationPut()
-        {
-            // Generated from example definition: specification/storage/resource-manager/Microsoft.Storage/stable/2024-01-01/examples/QueueOperationPut.json
-            // this example is just showing the usage of "Queue_Create" operation, for the dependent resources, they will have to be created separately.
-
-            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
-            TokenCredential cred = new DefaultAzureCredential();
-            // authenticate your client
-            ArmClient client = new ArmClient(cred);
-
-            // this example assumes you already have this QueueServiceResource created on azure
-            // for more information of creating QueueServiceResource, please refer to the document of QueueServiceResource
-            string subscriptionId = "{subscription-id}";
-            string resourceGroupName = "res3376";
-            string accountName = "sto328";
-            ResourceIdentifier queueServiceResourceId = QueueServiceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, accountName);
-            QueueServiceResource queueService = client.GetQueueServiceResource(queueServiceResourceId);
-
-            // get the collection of this StorageQueueResource
-            StorageQueueCollection collection = queueService.GetStorageQueues();
-
-            // invoke the operation
-            string queueName = "queue6185";
-            StorageQueueData data = new StorageQueueData();
-            ArmOperation<StorageQueueResource> lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, queueName, data);
-            StorageQueueResource result = lro.Value;
-
-            // the variable result is a resource, you could call other operations on this instance as well
-            // but just for demo, we get its data from this resource instance
-            StorageQueueData resourceData = result.Data;
-            // for demo we just print out the id
-            Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-        }
-
-        [Test]
-        [Ignore("Only validating compilation of examples")]
         public async Task CreateOrUpdate_QueueOperationPutWithMetadata()
         {
             // Generated from example definition: specification/storage/resource-manager/Microsoft.Storage/stable/2024-01-01/examples/QueueOperationPutWithMetadata.json
