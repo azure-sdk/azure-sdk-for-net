3079d4d46d851cf407f966890301b8655a6739d8
azure-sdk <azuresdk@microsoft.com>
2025-06-13 14:13:44 +0000

    Update SDK code cl_co_1

--- sdk/communication/Azure.Communication.Messages/src/Generated/UpdateParticipantsResult.Serialization.cs
+++ sdk/communication/Azure.Communication.Messages/src/Generated/UpdateParticipantsResult.Serialization.cs
@@ -39,7 +39,7 @@ namespace Azure.Communication.Messages
             if (Optional.IsDefined(Error))
             {
                 writer.WritePropertyName("error"u8);
-                JsonSerializer.Serialize(writer, Error);
+                ((IJsonModel<ResponseError>)Error).Write(writer, options);
             }
             if (options.Format != "W" && _serializedAdditionalRawData != null)
             {
@@ -95,7 +95,7 @@ namespace Azure.Communication.Messages
                     {
                         continue;
                     }
-                    error = JsonSerializer.Deserialize<ResponseError>(property.Value.GetRawText());
+                    error = ModelSerializationExtensions.JsonDeserialize<ResponseError>(property.Value.GetRawText(), ModelSerializationExtensions.Options);
                     continue;
                 }
                 if (options.Format != "W")
