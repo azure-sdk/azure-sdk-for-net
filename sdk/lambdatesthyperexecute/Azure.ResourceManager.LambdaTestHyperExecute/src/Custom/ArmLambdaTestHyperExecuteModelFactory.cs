571146ea81f219deea5abacbdbc477eae0b1d661
Arcturus Zhang <dapzhang@microsoft.com>
2026-04-27 13:42:25 +0800

    fix existing issues

--- sdk/lambdatesthyperexecute/Azure.ResourceManager.LambdaTestHyperExecute/src/Custom/ArmLambdaTestHyperExecuteModelFactory.cs
+++ sdk/lambdatesthyperexecute/Azure.ResourceManager.LambdaTestHyperExecute/src/Custom/ArmLambdaTestHyperExecuteModelFactory.cs
@@ -26,5 +26,29 @@ namespace Azure.ResourceManager.LambdaTestHyperExecute.Models
                 provisioningState,
                 partnerLicensesSubscribed.GetValueOrDefault(),
                 singleSignOnProperties);
+
+        // Workaround: The generator has a bug that when an overload is created in customized code with the same parameter list, but the nullability of some parameters is different,
+        // the generator somehow treat the overload as a duplicated method, and the generated overload with different nullability will no longer be generated.
+        /// <param name="marketplace"> Marketplace details of the resource. </param>
+        /// <param name="user"> Details of the user. </param>
+        /// <param name="provisioningState"> Provisioning state of the resource. </param>
+        /// <param name="partnerLicensesSubscribed"> The number of licenses subscribed. </param>
+        /// <param name="singleSignOnProperties"> Single sign-on properties. </param>
+        /// <returns> A new <see cref="Models.LambdaTestHyperExecuteOrganizationProperties"/> instance for mocking. </returns>
+        public static LambdaTestHyperExecuteOrganizationProperties LambdaTestHyperExecuteOrganizationProperties(
+            LambdaTestHyperExecuteMarketplaceDetails marketplace = default,
+            LambdaTestHyperExecuteUserDetails user = default,
+            LambdaTestHyperExecuteOfferProvisioningState? provisioningState = default,
+            int partnerLicensesSubscribed = default,
+            LambdaTestHyperExecuteSingleSignOnPropertiesV2 singleSignOnProperties = default)
+        {
+            return new LambdaTestHyperExecuteOrganizationProperties(
+                marketplace,
+                user,
+                provisioningState,
+                new LambdaTestHyperExecuteOfferPartnerProperties(partnerLicensesSubscribed, null),
+                singleSignOnProperties,
+                additionalBinaryDataProperties: null);
+        }
     }
 }
